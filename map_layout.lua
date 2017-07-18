1c62f974fa098bdf23e399a02a17a4e62581f5e0
Valansch <mail@maikwild.de>
2017-07-18 20:28:00 +0200

    Added donut and removed module boll call from map_layout.lua

--- map_layout.lua
+++ map_layout.lua
@@ -10,10 +10,11 @@ require "locale.gen_shared.simplex_noise"
 --shapes--
 --require "locale.gen_shape.right"
 --require "locale.gen_shape.up"
-require "locale.gen_shape.maze"
+--require "locale.gen_shape.maze"
 --require "locale.gen_shape.spiral"
 --require "locale.gen_shape.spiral_tri"
 --require "locale.gen_shape.spiral2"
+--require "locale.gen_shape.donut"
 
 --terrain--
 --require "locale.gen_terrain.neko_bridged_rivers"
@@ -25,18 +26,18 @@ require "locale.gen_shape.maze"
 --TODO: IMPLEMENT BROKEN STYLES. DO NOT MERGE INTO MASTER BEFORE THIS IS
 
 local on_chunk_generated = function(event)
-	if shape_module then
+	if run_shape_module ~= nil then
 		if run_shape_module(event) then
-			if terrain_module then
+			if run_terrain_module ~= nil then
 				run_terrain_module(event)
-			elseif ores_module then
+			elseif run_ores_module ~= nil then
 				run_ores_module(event)
 			end
 		end
 	else
-		if terrain_module then
+		if run_terrain_module ~= nil then
 			run_terrain_module(event)
-		elseif ores_module then
+		elseif run_ores_module ~= nil then
 			run_ores_module(event)
 		end
 	end
