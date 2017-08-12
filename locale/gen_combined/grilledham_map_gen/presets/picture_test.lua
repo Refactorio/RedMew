189b32337bb56e143dc2257c1234021608bb343b
James Gillham <jrdgillham@gmail.com>
2017-08-12 04:19:59 +0100

    preset changes

--- locale/gen_combined/grilledham_map_gen/presets/picture_test.lua
+++ locale/gen_combined/grilledham_map_gen/presets/picture_test.lua
@@ -1,15 +1,12 @@
 require "locale.gen_combined.grilledham_map_gen.map_gen"
 require "locale.gen_combined.grilledham_map_gen.builders"
 
-local pic = require "locale.gen_combined.grilledham_map_gen.data.crosses3"
+local pic = require "locale.gen_combined.grilledham_map_gen.data.color_mona_lisa"
 
-local scale_factor = 4
+local scale_factor = 1
 local shape = picture_builder(pic.data, pic.width, pic.height)
 shape = scale(shape, scale_factor, scale_factor)
---shape = rotate(shape, degrees(45))
-shape = invert(shape)
 
 local map = single_pattern_builder(shape, pic.width * scale_factor, pic.height * scale_factor)
-map = rotate(map, degrees(45))
 
 return map
