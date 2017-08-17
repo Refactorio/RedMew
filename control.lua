06a1daea36f0a6c1552c181421071b5eb0fc2849
James Gillham <jrdgillham@gmail.com>
2017-08-18 00:43:00 +0100

    new stuff

--- control.lua
+++ control.lua
@@ -43,6 +43,23 @@ function walkabout(player_name, distance)
 		game.player.print("This command moved to /walkabout.")
 end
 
+--[[
+tp_players = {}
+
+function tp_mode(boolean)	
+	tp_players[game.player.index] = boolean	
+end
+
+local function put_item(event)
+	local index = event.player_index
+	if tp_players[index] then
+		game.players[index].teleport(event.position)		
+	end
+end
+
+Event.register(defines.events.on_put_item, put_item )
+
+--]]
 
 Event.register(defines.events.on_player_created, player_joined)
 Event.register(defines.events.on_player_respawned, player_respawned)
