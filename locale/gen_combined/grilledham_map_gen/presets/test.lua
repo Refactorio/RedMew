cb772ab84ad89e70f57df247d64aa511acad2efd
James Gillham <jrdgillham@gmail.com>
2017-08-25 06:27:50 +0100

    new presets and bug fixes

--- locale/gen_combined/grilledham_map_gen/presets/test.lua
+++ locale/gen_combined/grilledham_map_gen/presets/test.lua
@@ -32,9 +32,45 @@ return map
 --]]
 
 
+--local shape = circle_builder(9)
+--local shape = oval_builder(9, 10)
+--local shape = rectangle_builder(10, 10)
+local pic = require "locale.gen_combined.grilledham_map_gen.data.goat"
+local shape = picture_builder(pic.data, pic.width, pic.height)
+--local shape = path_builder(2,4)
+--local shape = square_diamond_builder(7)
+--local shape = rotate(rectangle_builder(8,8), degrees(45))
 
-local shape = rectangle_builder(5, 5)
-shape = project(shape, 5, 3)
---shape = scale(shape, 1, 1)
+local shape = require "locale.gen_combined.grilledham_map_gen.presets.mobius_strip"
+
+--local shape = path_builder(4)
+--shape = compound_or({ shape, rotate(shape, degrees(45)) })
+
+
+--shape = single_pattern_builder(shape,64,64)
+--shape = project(shape, pic.height / 2 , 1.25)
+shape = project_overlap(shape, 128, 1.1875)
+
+local crop = translate(rectangle_builder(1000000,1000000), 0, -500000 - pic.height / 2)
+crop = invert(crop)
+shape = compound_and({ crop, shape })
+
+
+--shape = scale(shape, 0.125, 0.125)
+--shape = rotate(shape, degrees(45))
+
+shape = compound_or(
+    { 
+        shape,
+        rotate(shape, degrees(45)),
+        rotate(shape, degrees(90)), 
+        rotate(shape, degrees(135)), 
+        rotate(shape, degrees(180)), 
+        rotate(shape, degrees(225)), 
+        rotate(shape, degrees(270)),
+        rotate(shape, degrees(315)),        
+    })
+
+shape = scale(shape, 0.125, 0.125)
 
 return shape
