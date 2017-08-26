a7c9e0a2baa329895b227143db242f0a904b869a
James Gillham <jrdgillham@gmail.com>
2017-08-27 00:29:17 +0100

    changes to mobius_strip

--- locale/gen_combined/grilledham_map_gen/presets/mobius_strip.lua
+++ locale/gen_combined/grilledham_map_gen/presets/mobius_strip.lua
@@ -3,25 +3,25 @@ require "locale.gen_combined.grilledham_map_gen.builders"
 
 local inner_circle = invert(circle_builder(48))
 local outer_circle = circle_builder(64)
-local square = invert(rectangle_builder(68,68))
+local square = invert(rectangle_builder(1000,1000))
 square = rotate(square, degrees(45))
-square = translate(square, 56,0)
+square = translate(square, math.sqrt(2) * 500,0)
 
 local circle = compound_and({ inner_circle, outer_circle, square })
 
-local line1 = rectangle_builder(66,16)
+local line1 = rectangle_builder(77,16)
 line1 = rotate(line1, degrees(45))
-line1 = translate(line1,66.5,12.25)
+line1 = translate(line1,66.5,12.6875)
 
 local line2 = rectangle_builder(46, 16)
 local line2 = rotate(line2, degrees(-45))
-line2 = translate(line2, 55.5,-23.5)
+line2 = translate(line2, 55.5,-23.6875)
 
 --line2 =change_tile(line2, true, "water")
 
 local half = compound_or({ line2,line1,circle})
 
-half = translate(half, -78.71875, 0)
+half = translate(half, -79.1875, 0)
 local map = compound_or({ half, flip_xy(half) })
 
 map = scale(map, 16, 16)
