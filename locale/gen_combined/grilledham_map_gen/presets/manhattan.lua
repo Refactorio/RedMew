05a0d9d049646d6c5072b343fcbbfc45efed3118
James Gillham <jrdgillham@gmail.com>
2017-08-29 10:28:45 +0100

    updates to manhattan map

--- locale/gen_combined/grilledham_map_gen/presets/manhattan.lua
+++ locale/gen_combined/grilledham_map_gen/presets/manhattan.lua
@@ -4,5 +4,10 @@ require "locale.gen_combined.grilledham_map_gen.builders"
 local pic = require "locale.gen_combined.grilledham_map_gen.data.manhattan"
 
 local shape = picture_builder(pic.data, pic.width, pic.height)
+shape = translate(shape, 10, -96)
+shape = scale(shape,2,2)
+shape = rotate(shape, degrees(-22.5))
+
+shape = change_tile(shape, false, "deepwater")
 
 return shape
