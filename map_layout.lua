059a80bbe63dfd5098071a9ddf8e1f823f38112f
Valansch <mail@maikwild.de>
2017-10-06 02:14:38 +0200

    Implemented worms

--- map_layout.lua
+++ map_layout.lua
@@ -35,6 +35,7 @@ in this file and your run_*type*_module(event) function will be called.
 --terrain--
 --require "locale.gen_terrain.neko_bridged_rivers"
 --require "locale.gen_terrain.neko_river_overlay"
+--require "locale.gen_terrain.worms"
 
 --ores--
 --require "locale.gen_ores.neko_crazy_ores"
