f1b11d2e4770a800206b8fc8a49783ae442001a8
TWLTriston <triston@gmail.com>
2017-10-25 06:41:20 -0400

    Removed the forced include of dagobah

--- map_layout.lua
+++ map_layout.lua
@@ -10,7 +10,7 @@ in this file and your run_*type*_module(event) function will be called.
 --require "locale.gen_combined.island_resort"
 --require "locale.gen_combined.red_planet_v2"
 --require "locale.gen_combined.borg_planet_v2"
-require "locale.gen_combined.dagobah_swamp"
+--require "locale.gen_combined.dagobah_swamp"
 
 --grilledham's maps
 --MAP_GEN = require "locale.gen_combined.grilledham_map_gen.presets.mobius_strip"
