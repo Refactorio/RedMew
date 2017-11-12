1a770ecca2fc292f84fdaac5b0bbf7a1d09f1c09
TWLTriston <triston@gmail.com>
2017-11-12 16:30:17 -0500

    Grass tiles now get dagobah swamp based values

--- locale/gen_combined/grilledham_map_gen/presets/biome_test.lua
+++ locale/gen_combined/grilledham_map_gen/presets/biome_test.lua
@@ -1,9 +1,11 @@
 require "locale.gen_combined.grilledham_map_gen.map_gen"
 
+map_gen_decoratives = true
+
 local pic = require "locale.gen_combined.grilledham_map_gen.data.biome_test"
 
 local shape = picture_builder(pic.data, pic.width, pic.height)
 
-shape = change_tile(shape, false, "out-of-map")
+--shape = change_tile(shape, false, "out-of-map")
 
 return shape
