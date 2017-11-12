2d16463cb0a6643bd3cdd91712d5c60e69f1442f
TWLTriston <triston@gmail.com>
2017-11-12 11:41:30 -0500

    Multi row and column representation for all tile types.

--- locale/gen_combined/grilledham_map_gen/presets/biome_test.lua
+++ locale/gen_combined/grilledham_map_gen/presets/biome_test.lua
@@ -1,10 +1,9 @@
 require "locale.gen_combined.grilledham_map_gen.map_gen"
-require "locale.gen_combined.grilledham_map_gen.builders"
 
 local pic = require "locale.gen_combined.grilledham_map_gen.data.biome_test"
 
 local shape = picture_builder(pic.data, pic.width, pic.height)
 
-shape = change_tile(shape, false, "deepwater")
+shape = change_tile(shape, false, "out-of-map")
 
 return shape
