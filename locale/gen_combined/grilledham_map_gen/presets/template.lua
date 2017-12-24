b92ac4e95b4119ecf6458e9c76527d4731abd1c6
grilledham <jrdgillham@gmail.com>
2017-12-24 16:07:07 +0000

    Added template

---
+++ locale/gen_combined/grilledham_map_gen/presets/template.lua
@@ -0,0 +1,17 @@
+--require "locale.gen_combined.grilledham_map_gen.map_gen_no_thread"
+require "locale.gen_combined.grilledham_map_gen.map_gen"
+
+local pic = require "locale.gen_combined.grilledham_map_gen.data.template"
+local pic = decompress(pic)
+local map = picture_builder(pic)
+
+-- this changes the size of the map
+--map = scale(map, 2, 2)
+
+-- this moves the map, effectively changing the spawn point.
+--map = translate(map, 0, -200)
+
+-- this sets the tile outside the bounds of the map to deepwater, remove this and it will be void.
+--map = change_tile(map, false, "deepwater")
+
+return map
--- map_layout.lua
+++ map_layout.lua
@@ -18,6 +18,7 @@ in this file and your run_*type*_module(event) function will be called.
 -- Need to copy the file you want from the _locale folder to this one for it to be included
 -- only get what you need, otherwise the save file is too big!
 
+--MAP_GEN = require "locale.gen_combined.grilledham_map_gen.presets.template"
 --MAP_GEN = require "locale.gen_combined.grilledham_map_gen.presets.mobius_strip"
 --MAP_GEN = require "locale.gen_combined.grilledham_map_gen.presets.antfarm"
 --MAP_GEN = require "locale.gen_combined.grilledham_map_gen.presets.creation_of_adam"
