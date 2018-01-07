562bd6bd5ee022b2ede801b1e0a37be295fe32a2
Valansch <mail@maikwild.de>
2018-01-07 22:28:00 +0100

    removed deprecated user groups code again?

--- user_groups.lua
+++ user_groups.lua
@@ -1,7 +1,7 @@
 function update_group(position)
 	local file = position .. ".lua"
 	game.write_file(file, "{", false, 0)
-	local group = global.scenario.variables[position]
+	local group = global.position]
 	local line = ""
 	for player_name,_ in pairs(group) do
 		line = string.format('["%s"] = "",\n', player_name)
@@ -16,11 +16,11 @@ function get_actor()
 end
 
 function is_mod(player_name)
-	return global.scenario.variables.mods[player_name:lower()]
+	return global.mods[player_name:lower()]
 end
 
 function is_regular(player_name)
-	return global.scenario.variables.regulars[player_name:lower()]
+	return global.regulars[player_name:lower()]
 end
 
 function add_regular(player_name)
@@ -29,7 +29,7 @@ function add_regular(player_name)
     else
         if game.players[player_name] then
             game.print(actor .. " promoted " .. player_name .. " to regular.")
-            global.scenario.variables.regulars[player_name:lower()] = ""
+            global.regulars[player_name:lower()] = ""
             update_group("regulars")
         else
             player_print(player_name .. " does not exist.")
@@ -43,7 +43,7 @@ function add_mod(player_name)
     else
         if game.players[player_name] then
             game.print(actor .. " promoted " .. player_name .. " to moderator.")
-            global.scenario.variables.mods[player_name:lower()] = ""
+            global.mods[player_name:lower()] = ""
             update_group("mods")
         else
             player_print(player_name .. " does not exist.")
@@ -54,25 +54,25 @@ end
 function remove_regular(player_name)
 	local actor = get_actor()
 	if is_regular(player_name) then game.print(player_name .. " was demoted from regular by " .. actor .. ".") end
-	global.scenario.variables.regulars[player_name] = nil
+	global.regulars[player_name] = nil
 	update_group("regulars")
 end
 
 function remove_mod(player_name)
 	local actor = get_actor()
 	if is_mod(player_name) then game.print(player_name .. " was demoted from mod by " .. actor .. ".") end
-	global.scenario.variables.mods[player_name] = nil
+	global.mods[player_name] = nil
 	update_group("mods")
 end
 
 function print_regulars()
-	for k,_ in pairs(global.scenario.variables.regulars) do
+	for k,_ in pairs(global.regulars) do
 		player_print(k)
 	end
 end
 
 function print_mods()
-	for k,_ in pairs(global.scenario.variables.mods) do
+	for k,_ in pairs(global.mods) do
 		player_print(k)
 	end
 end
