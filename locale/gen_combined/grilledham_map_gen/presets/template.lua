ad1877ece7036a50955aa432b4c959147a8029a0
grilledham <jrdgillham@gmail.com>
2018-01-15 17:23:36 +0000

    Added helpful comments to template

--- locale/gen_combined/grilledham_map_gen/presets/template.lua
+++ locale/gen_combined/grilledham_map_gen/presets/template.lua
@@ -1,4 +1,8 @@
---require "locale.gen_combined.grilledham_map_gen.map_gen_no_thread"
+map_gen_decoratives = false -- Generate our own decoratives
+map_gen_rows_per_tick = 4 -- Inclusive integer between 1 and 32. Used for map_gen_threaded, higher numbers will generate map quicker but cause more lag.
+
+-- Recommend to use map_gen, but map_gen_not_threaded may be useful for testing / debugging.
+--require "locale.gen_combined.grilledham_map_gen.map_gen_not_threaded"
 require "locale.gen_combined.grilledham_map_gen.map_gen"
 
 local pic = require "locale.gen_combined.grilledham_map_gen.data.template"
