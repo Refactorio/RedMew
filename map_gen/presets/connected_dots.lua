e4d6a7ca333964f2ded0c844c1c6b6861e4aec7a
grilledham <jrdgillham@gmail.com>
2018-01-21 15:31:20 +0000

    updates to old maps

--- map_gen/presets/connected_dots.lua
+++ map_gen/presets/connected_dots.lua
@@ -1,5 +1,9 @@
+map_gen_decoratives = false -- Generate our own decoratives
+map_gen_rows_per_tick = 8 -- Inclusive integer between 1 and 32. Used for map_gen_threaded, higher numbers will generate map quicker but cause more lag.
+
+-- Recommend to use generate, but generate_not_threaded may be useful for testing / debugging.
+--require "map_gen.shared.generate_not_threaded"
 require "map_gen.shared.generate"
-require "map_gen.shared.builders"
 
 local function no_resources(x, y, world_x, world_y, tile, entity)
     local surface = MAP_GEN_SURFACE
--- map_gen/presets/goat.lua
+++ map_gen/presets/goat.lua
@@ -1,5 +1,9 @@
+map_gen_decoratives = false -- Generate our own decoratives
+map_gen_rows_per_tick = 8 -- Inclusive integer between 1 and 32. Used for map_gen_threaded, higher numbers will generate map quicker but cause more lag.
+
+-- Recommend to use generate, but generate_not_threaded may be useful for testing / debugging.
+--require "map_gen.shared.generate_not_threaded"
 require "map_gen.shared.generate"
-require "map_gen.shared.builders"
 
 local pic = require "map_gen.data.presets.goat"
 
