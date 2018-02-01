d941755eeba2dc39ea2856b9fbc01a16984120d8
Toledini <35410339+Toledin@users.noreply.github.com>
2018-02-01 14:15:38 -0500

    changed function players() to bot_command_players()

--- server_api.lua
+++ server_api.lua
@@ -1,4 +1,4 @@
-function players(filename)
+function bot_command_players(filename)
     game.write_file(filename, string.format("Online players (%d):",#game.connected_players), false,0)
     local online_players = #game.connected_players
     for _,p in pairs(online_players) do
