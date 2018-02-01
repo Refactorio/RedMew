ddd69013c0b54809f0fc65b1b116a819ffd018f8
Toledini <35410339+Toledin@users.noreply.github.com>
2018-02-01 10:07:32 -0500

    chnaged get_online_players() to players()

--- server_api.lua
+++ server_api.lua
@@ -1,4 +1,4 @@
-function get_online_players(filename)
+function players(filename)
     game.write_file(filename, string.format("Online players (%d):",#game.connected_players), false,0)
     local online_players = #game.connected_players
     for _,p in pairs(online_players) do
