b47344c3228813d0496853acc6eca5e6b2a79fd8
grilledham <jrdgillham@gmail.com>
2018-02-04 22:12:36 +0000

    hearts map

---
+++ map_gen/presets/hearts.lua
@@ -0,0 +1,21 @@
+-- Recommend to use generate, but generate_not_threaded may be useful for testing / debugging.
+require "map_gen.shared.generate_not_threaded"
+--require "map_gen.shared.generate"
+
+local circle = circle_builder(16)
+local square = rectangle_builder(30)
+square = rotate(square, degrees(45))
+
+local heart = compound_or{translate(circle, -14, 0), translate(circle, 14, 0), translate(square, 0, 14)}
+--local hollow_heart = compound_and{invert(heart), scale(heart, 2, 2)}
+
+heart = translate(heart, 0, -10)
+heart = scale(heart, 51/60, 1)
+local hearts = grow(heart, heart, 52, 0.5)
+
+local line = line_y_builder(4)
+
+map = compound_or{line, hearts}
+--map = scale(map, 6, 6)
+
+return map
