6e5c2f2a63d3d72f3375f9a3556bda09dc0a5db1
Rickeyhb <Rickeyhb@users.noreply.github.com>
2018-04-12 21:55:07 +0200

    added plus map

---
+++ map_gen/presets/plus.lua
@@ -0,0 +1,39 @@
+--map_gen_decoratives = false -- Generate our own decoratives
+--map_gen_rows_per_tick = 4 -- Inclusive integer between 1 and 32. Used for map_gen_threaded, higher numbers will generate map quicker but cause more lag.
+--
+---- Recommend to use generate, but generate_not_threaded may be useful for testing / debugging.
+----require "map_gen.shared.generate_not_threaded"
+--require "map_gen.shared.generate"
+--
+--local pic = require "map_gen.data.presets.plus"
+--local pic = decompress(pic)
+--local map = picture_builder(pic)
+--
+---- this changes the size of the map
+----map = scale(map, 2, 2)
+--
+---- this moves the map, effectively changing the spawn point.
+----map = translate(map, 0, -200)
+--
+---- this sets the tile outside the bounds of the map to deepwater, remove this and it will be void.
+----map = change_tile(map, false, "deepwater")
+--
+--return map
+
+
+-- Recommend to use generate, but generate_not_threaded may be useful for testing / debugging.
+require "map_gen.shared.generate_not_threaded"
+--require "map_gen.shared.generate"
+
+local pic = require "map_gen.data.presets.plus" --or whatever you called it in data->presets
+local pic = decompress(pic)
+local map = picture_builder(pic)
+
+map = single_pattern_builder(map, pic.width-1, pic.height-1)
+
+map = translate(map, 86, 0)
+
+-- uncomment the line below to change the size of the map scale(x, y)
+--map = scale(map, 2, 2)
+
+return map
