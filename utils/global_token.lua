5fb107e20f0ae27f52bc8accb683378423f6a6f8
grilledham <jrdgillham@gmail.com>
2018-05-16 11:37:22 +0100

    added global tokens

--- utils/global_token.lua
+++ utils/global_token.lua
@@ -17,4 +17,18 @@ function Token.get(token_id)
     return tokens[token_id]
 end
 
+global.tokens = {}
+
+function Token.register_global(var)
+    local c = #global.tokens + 1
+
+    global.tokens[c] = var
+
+    return c
+end
+
+function Token.get_global(token_id)
+    return global.tokens[token_id]
+end
+
 return Token
