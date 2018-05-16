1cee784b54aca3832c9957def9ac1697b30376c5
grilledham <jrdgillham@gmail.com>
2018-05-16 11:36:12 +0100

    added regulars and mods

---
+++ resources/mods.lua
@@ -0,0 +1,12 @@
+return {
+    ["dpoba"] = "",
+    ["masjazz"] = "",
+    ["mbas"] = "",
+    ["selen"] = "",
+    ["sholvo"] = "",
+    ["animal"] = "",
+    ["terarink"] = "",
+    ["theorangeangle"] = "",
+    ["fabilord98"] = "",
+    ["rickeyhb"] = ""
+}
--- user_groups.lua
+++ user_groups.lua
@@ -1,12 +1,12 @@
-global.mods = {}
-global.regulars = {}
+global.regulars = require "resources.regulars"
+global.mods = require "resources.mods"
 
 function update_group(position)
 	local file = position .. ".lua"
 	game.write_file(file, "{", false, 0)
 	local group = global[position]
 	local line = ""
-	for player_name,_ in pairs(group) do
+	for player_name, _ in pairs(group) do
 		line = string.format('["%s"] = "",\n', player_name)
 		game.write_file(file, line, true, 0)
 	end
@@ -14,7 +14,9 @@ function update_group(position)
 end
 
 function get_actor()
-	if game.player then return game.player.name end
+	if game.player then
+		return game.player.name
+	end
 	return "<server>"
 end
 
@@ -27,55 +29,61 @@ function is_regular(player_name)
 end
 
 function add_regular(player_name)
-		local actor = get_actor()
-    if is_regular(player_name) then player_print(player_name .. " is already a regular.")
-    else
-        if game.players[player_name] then
-            game.print(actor .. " promoted " .. player_name .. " to regular.")
-            global.regulars[player_name:lower()] = ""
-            update_group("regulars")
-        else
-            player_print(player_name .. " does not exist.")
-        end
-    end
+	local actor = get_actor()
+	if is_regular(player_name) then
+		player_print(player_name .. " is already a regular.")
+	else
+		if game.players[player_name] then
+			game.print(actor .. " promoted " .. player_name .. " to regular.")
+			global.regulars[player_name:lower()] = ""
+			update_group("regulars")
+		else
+			player_print(player_name .. " does not exist.")
+		end
+	end
 end
 
 function add_mod(player_name)
-		local actor = get_actor()
-    if is_mod(player_name) then player_print(player_name .. " is already a moderator.")
-    else
-        if game.players[player_name] then
-            game.print(actor .. " promoted " .. player_name .. " to moderator.")
-            global.mods[player_name:lower()] = ""
-            update_group("mods")
-        else
-            player_print(player_name .. " does not exist.")
-        end
-    end
+	local actor = get_actor()
+	if is_mod(player_name) then
+		player_print(player_name .. " is already a moderator.")
+	else
+		if game.players[player_name] then
+			game.print(actor .. " promoted " .. player_name .. " to moderator.")
+			global.mods[player_name:lower()] = ""
+			update_group("mods")
+		else
+			player_print(player_name .. " does not exist.")
+		end
+	end
 end
 
 function remove_regular(player_name)
 	local actor = get_actor()
-	if is_regular(player_name) then game.print(player_name .. " was demoted from regular by " .. actor .. ".") end
+	if is_regular(player_name) then
+		game.print(player_name .. " was demoted from regular by " .. actor .. ".")
+	end
 	global.regulars[player_name] = nil
 	update_group("regulars")
 end
 
 function remove_mod(player_name)
 	local actor = get_actor()
-	if is_mod(player_name) then game.print(player_name .. " was demoted from mod by " .. actor .. ".") end
+	if is_mod(player_name) then
+		game.print(player_name .. " was demoted from mod by " .. actor .. ".")
+	end
 	global.mods[player_name] = nil
 	update_group("mods")
 end
 
 function print_regulars()
-	for k,_ in pairs(global.regulars) do
+	for k, _ in pairs(global.regulars) do
 		player_print(k)
 	end
 end
 
 function print_mods()
-	for k,_ in pairs(global.mods) do
+	for k, _ in pairs(global.mods) do
 		player_print(k)
 	end
 end
