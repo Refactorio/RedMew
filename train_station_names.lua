3b338fe6f5a2498c7d9bb13317089c8709ee5336
grilledham <jrdgillham@gmail.com>
2018-05-19 13:53:38 +0100

    fixed bug

--- train_station_names.lua
+++ train_station_names.lua
@@ -1,18 +1,20 @@
-local Event = require "utils.event"
+local Event = require 'utils.event'
 
 local function player_built_entity(event)
+    local entity = event.created_entity
+    if not entity or not entity.valid then
+        return
+    end
 
-	if event.created_entity.name == "train-stop" then
-		local y = math.random(1,3)
-		if y == 1 then			
-		else
-			local total_players = #game.players
-			local x = math.random(1,total_players)
-			local player = game.players[x]
-			event.created_entity.backer_name = player.name
-		end
-	end
-	
+    if entity.name == 'train-stop' then
+        local y = math.random(1, 3)
+        if y ~= 1 then
+            local total_players = #game.players
+            local x = math.random(1, total_players)
+            local player = game.players[x]
+            event.created_entity.backer_name = player.name
+        end
+    end
 end
 
 Event.add(defines.events.on_built_entity, player_built_entity)
