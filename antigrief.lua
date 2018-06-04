daed9da30b0f40231d16d56339b900553f7eb9c3
Valansch <mail@maikwild.de>
2018-06-04 18:40:06 +0200

    fixed undo desync

--- antigrief.lua
+++ antigrief.lua
@@ -1,13 +1,13 @@
---local Event = require "utils.event"
-local surface = {}
+local Event = require "utils.event"
+
 
 Event.on_init(function()
-  surface = game.create_surface("antigrief", {  autoplace_controls = {    coal = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["copper-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["crude-oil"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    desert = {      frequency = "normal",      richness = "normal",      size = "none"    },    dirt = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["enemy-base"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    grass = {      frequency = "normal",      richness = "normal",      size = "very-high"    },    ["iron-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    sand = {      frequency = "normal",      richness = "normal",      size = "none"    },    stone = {      frequency = "normal",      richness = "normal",      size = "none"    },    trees = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["uranium-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    }  },  cliff_settings = {    cliff_elevation_0 = 1024,    cliff_elevation_interval = 10,    name = "cliff"  },  height = 2000000,  peaceful_mode = false,  seed = 3461559752,  starting_area = "very-low",  starting_points = {    {      x = 0,      y = 0    }  },  terrain_segmentation = "normal",  water = "none",  width = 2000000})
-  surface.always_day = true
+  global.ag_surface = game.create_surface("antigrief", {  autoplace_controls = {    coal = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["copper-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["crude-oil"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    desert = {      frequency = "normal",      richness = "normal",      size = "none"    },    dirt = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["enemy-base"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    grass = {      frequency = "normal",      richness = "normal",      size = "very-high"    },    ["iron-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    },    sand = {      frequency = "normal",      richness = "normal",      size = "none"    },    stone = {      frequency = "normal",      richness = "normal",      size = "none"    },    trees = {      frequency = "normal",      richness = "normal",      size = "none"    },    ["uranium-ore"] = {      frequency = "normal",      richness = "normal",      size = "none"    }  },  cliff_settings = {    cliff_elevation_0 = 1024,    cliff_elevation_interval = 10,    name = "cliff"  },  height = 2000000,  peaceful_mode = false,  seed = 3461559752,  starting_area = "very-low",  starting_points = {    {      x = 0,      y = 0    }  },  terrain_segmentation = "normal",  water = "none",  width = 2000000})
+  global.ag_surface.always_day = true
+ 
 end)
 
 local function place_entity_on_surface(entity, surface, replace, player)
-  if entity.surface == surface then return end
   if replace then
     for _,e in pairs(surface.find_entities_filtered{position = entity.position}) do
       e.destroy()
@@ -49,24 +49,24 @@ end)
 
 Event.add(defines.events.on_robot_pre_mined, function(event) 
   if event.entity.force.name == "player" and event.entity.last_user then
-    place_entity_on_surface(event.entity, true, surface, event.entity.last_user) 
+    place_entity_on_surface(event.entity, true, global.ag_surface, event.entity.last_user) 
   end
 end)
 
 Event.add(defines.events.on_player_mined_entity, function(event)
-  place_entity_on_surface(event.entity, surface, true, event.player_index)
+  place_entity_on_surface(event.entity, global.ag_surface, true, event.player_index)
 end)
 
 local Module = {}
 
 Module.undo = function(player)
   if type(player) == "nil" or type(player) == "string" then return end --No support for strings!
-  if type(player) == "number" then 
-    local player = game.players[player]
-  end
-  for _,e in pairs(surface.find_entities_filtered{}) do
-    if e.last_user == player then
+  for _,e in pairs(global.ag_surface.find_entities_filtered{}) do
+    if e.last_user == player  or e.last_user.index == player then
       place_entity_on_surface(e, game.surfaces.nauvis, false) 
+      for _,f in pairs(global.ag_surface.find_entities_filtered{position = e.position}) do
+      	f.destroy()
+      end
     end
   end
 end
