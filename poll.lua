1f2e64ab8a233fcff64f675b078ccedf412a697f
grilledham <jrdgillham@gmail.com>
2018-06-17 14:51:06 +0100

    poll rewrite

--- poll.lua
+++ poll.lua
@@ -3,9 +3,155 @@
 -- by MewMew -- with some help from RedLabel, Klonan, Morcup, BrainClot
 ----------------------------------------------------------------------------------------------------------------------------------------
 
-local Event = require "utils.event"
+local Gui = require 'utils.gui'
+local Global = require 'utils.global'
+local Event = require 'utils.event'
 
-local function create_poll_gui(event)
+local polls = {}
+local no_notify_players = {}
+
+Global.register(
+    {polls = polls, no_notify_players = no_notify_players},
+    function(tbl)
+        polls = tbl.polls
+        no_notify_players = tbl.no_notify_players
+    end
+)
+
+local main_button_name = Gui.uid_name()
+local main_frame_name = Gui.uid_name()
+local create_poll_button_name = Gui.uid_name()
+local notify_checkbox_name = Gui.uid_name()
+
+local create_poll_frame_name = Gui.uid_name()
+local create_poll_label_name = Gui.uid_name()
+local create_poll_textfield_name = Gui.uid_name()
+local create_poll_close_name = Gui.uid_name()
+local create_poll_confirm_name = Gui.uid_name()
+
+local function player_joined(event)
+    local player = game.players[event.player_index]
+    if not player or not player.valid then
+        return
+    end
+
+    if player.gui.top[main_button_name] ~= nil then
+        return
+    end
+
+    player.gui.top.add {type = 'sprite-button', name = main_button_name, sprite = 'item/programmable-speaker'}
+end
+
+local function draw_main_frame(left, player)
+    local frame = left.add {type = 'frame', name = main_frame_name, caption = 'Polls'}
+
+    local bottom_flow = frame.add {type = 'flow', direction = 'horizontal'}
+
+    local left_flow = bottom_flow.add {type = 'flow'}
+    left_flow.style.align = 'left'
+    left_flow.style.horizontally_stretchable = true
+
+    left_flow.add {type = 'button', name = main_button_name, caption = 'Close'}
+
+    local right_flow = bottom_flow.add {type = 'flow'}
+    right_flow.style.align = 'right'
+
+    right_flow.add {type = 'button', name = create_poll_button_name, caption = 'Create Poll'}
+
+    right_flow.add {
+        type = 'checkbox',
+        name = notify_checkbox_name,
+        caption = 'Show Polls',
+        state = not no_notify_players[player.index],
+        tooltip = 'Notify me when new polls are created'
+    }
+end
+
+local function toggle(event)
+    local left = event.player.gui.left
+    local main_frame = left[main_frame_name]
+
+    if main_frame then
+        Gui.remove_data_recursivly(main_frame)
+        main_frame.destroy()
+
+        local create_poll_frame = left[create_poll_frame_name]
+        if create_poll_frame and create_poll_frame.valid then
+            Gui.remove_data_recursivly(create_poll_frame)
+            create_poll_frame.destroy()
+        end
+    else
+        draw_main_frame(left, event.player)
+    end
+end
+
+local function draw_create_poll_frame(event)
+    local left = event.player.gui.left
+
+    local frame = left.add {type = 'frame', name = create_poll_frame_name, caption = 'New Poll', direction = 'vertical'}
+
+    local scroll_pane = frame.add {type = 'scroll-pane', direction = 'vertical', vertical_scroll_policy = 'always'}
+    scroll_pane.style.maximal_height = 500
+    scroll_pane.style.maximal_width = 400
+
+    local list = scroll_pane.add {type = 'flow', direction = 'vertical'}
+    list.style.horizontally_stretchable = true
+
+    local question_flow = list.add {type = 'flow', direction = 'horizontal'}
+    question_flow.add {type = 'label', name = create_poll_label_name, caption = 'Question'}
+    local textfield = question_flow.add {type = 'textfield', name = create_poll_textfield_name}
+    textfield.style.width = 600
+
+    local bottom_flow = frame.add {type = 'flow', direction = 'horizontal'}
+
+    local left_flow = bottom_flow.add {type = 'flow'}
+    left_flow.style.align = 'left'
+    left_flow.style.horizontally_stretchable = true
+
+    local close_button = left_flow.add {type = 'button', name = create_poll_close_name, caption = 'Close'}
+    Gui.set_data(close_button, frame)
+
+    local right_flow = bottom_flow.add {type = 'flow'}
+    right_flow.style.align = 'right'
+
+    right_flow.add {type = 'button', name = create_poll_confirm_name, caption = 'Create Poll'}
+end
+
+Event.add(defines.events.on_player_joined_game, player_joined)
+
+Gui.on_click(main_button_name, toggle)
+
+Gui.on_click(create_poll_button_name, draw_create_poll_frame)
+
+Gui.on_click(
+    create_poll_close_name,
+    function(event)
+        local element = event.element
+        local frame = Gui.get_data(element)
+
+        Gui.remove_data_recursivly(frame)
+        frame.destroy()
+    end
+)
+
+Gui.on_click(
+    notify_checkbox_name,
+    function(event)
+        local player_index = event.player_index
+        local checkbox = event.element
+
+        local new_state
+        if checkbox.state then
+            new_state = nil
+        else
+            new_state = true
+        end
+
+        no_notify_players[player_index] = new_state
+    end
+)
+
+--[[ local function create_poll_gui(event)
 	local player = game.players[event.player_index]
 
 	if player.gui.top.poll == nil then
@@ -323,3 +469,4 @@ Event.on_nth_tick(61, on_second)
 Event.add(defines.events.on_gui_click, on_gui_click)
 Event.add(defines.events.on_player_joined_game, create_poll_gui)
 Event.add(defines.events.on_player_joined_game, poll_sync_for_new_joining_player)
+ ]]
