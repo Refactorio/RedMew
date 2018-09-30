a8dd4e8a62f56427b9a831631d6d237046c7e389
Maik Wild <mail@maikwild.de>
2018-09-30 17:14:45 +0200

    threaded Template.lua

--- map_gen/Diggy/Template.lua
+++ map_gen/Diggy/Template.lua
@@ -1,5 +1,11 @@
+local Task = require 'utils.Task'
+local Token = require 'utils.global_token'
+
 -- this
 local Template = {}
+
+local tiles_per_call = 5 --how many tiles are inserted with each call of insert_action
+local entities_per_call = 5  --how many entities are inserted with each call of insert_action
 
 Template.events = {
     --[[--
@@ -23,6 +29,89 @@ Template.events = {
     on_void_removed = script.generate_event_name(),
 }
 
+local function min(a,b)
+    if a < b then return a end
+    return b
+end
+
+local function insert_next_tiles(data)
+    local void_removed = {}
+    local void_added = {}
+    local surface = data.surface
+    local tiles = {}
+
+    pcall(function() --use pcall to assure tile_iterator is always incremented, to avoid endless loops
+        for i = data.tile_iterator, min(data.tile_iterator + tiles_per_call - 1, data.tiles_n)   do
+            local new_tile = data.tiles[i]
+            table.insert(tiles, new_tile)
+            local current_tile = surface.get_tile(new_tile.position.x, new_tile.position.y)
+            local current_is_void = current_tile.name == 'out-of-map'
+            local new_is_void = new_tile.name == 'out-of-map'
+
+            if (current_is_void and not new_is_void) then
+                table.insert(void_removed, {surface = surface, old_tile = {name = current_tile.name, position = current_tile.position}})
+            end
+
+            if (new_is_void and not current_is_void) then
+                table.insert(void_added, {surface = surface, old_tile = {name = current_tile.name, position = current_tile.position}})
+            end
+        end
+    end)
+
+    data.tile_iterator = data.tile_iterator + tiles_per_call
+
+    surface.set_tiles(tiles)
+
+    for _, event in pairs(void_removed) do
+        script.raise_event(Template.events.on_void_removed, event)
+    end
+
+    for _, event in pairs(void_added) do
+        script.raise_event(Template.events.on_void_added, event)
+    end
+end
+
+local function insert_next_entities(data)
+    local created_entities = {}
+    local surface = data.surface
+
+    pcall(function() --use pcall to assure tile_iterator is always incremented, to avoid endless loops
+        for i = data.entity_iterator, min(data.entity_iterator + entities_per_call - 1, data.entities_n)   do
+            local entity = data.entities[i]
+            created_entity = surface.create_entity(entity)
+            if (nil == created_entity) then
+                error('Failed creating entity ' .. entity.name .. ' on surface.')
+            end
+
+            if ('sand-rock-big' == created_entity.name) then
+                created_entity.destructible = false
+            end
+
+            table.insert(created_entities, created_entity)
+        end
+    end)
+
+    data.entity_iterator = data.entity_iterator + entities_per_call
+
+    for _, entity in pairs(created_entities) do
+        script.raise_event(Template.events.on_placed_entity, {entity = entity})
+    end
+
+    return data.entity_iterator <= data.entities_n
+end
+
+local function insert_action(data)
+    if data.tile_iterator <= data.tiles_n then
+        insert_next_tiles(data)
+        return true
+    else
+        return insert_next_entities(data)
+    end
+
+end
+
+local insert_token = Token.register(insert_action)
+
 --[[--
     Inserts a batch of tiles and then entities.
 
@@ -34,51 +123,23 @@ Template.events = {
     @param entities table of entities as required by create_entity
 ]]
 function Template.insert(surface, tiles, entities)
-    local void_removed = {}
-    local void_added = {}
 
-    for _, new_tile in pairs(tiles) do
-        local current_tile = surface.get_tile(new_tile.position.x, new_tile.position.y)
-        local current_is_void = current_tile.name == 'out-of-map'
-        local new_is_void = new_tile.name == 'out-of-map'
+    local tiles = tiles or {}
+    local entities = entities or {}
+    local tiles_n = #tiles
+    local entities_n = #entities
+    local total_calls = math.ceil(tiles_n / tiles_per_call) + (entities_n / entities_per_call)
+    local data = {
+        tiles_n = tiles_n,
+        tile_iterator = 1,
+        entities_n = entities_n,
+        entity_iterator = 1,
+        surface = surface,
+        tiles = tiles,
+        entities = entities
+    }
 
-        if (current_is_void and not new_is_void) then
-            table.insert(void_removed, {surface = surface, old_tile = {name = current_tile.name, position = current_tile.position}})
-        end
-
-        if (new_is_void and not current_is_void) then
-            table.insert(void_added, {surface = surface, old_tile = {name = current_tile.name, position = current_tile.position}})
-        end
-    end
-
-    surface.set_tiles(tiles)
-
-    for _, event in pairs(void_removed) do
-        script.raise_event(Template.events.on_void_removed, event)
-    end
-
-    for _, event in pairs(void_added) do
-        script.raise_event(Template.events.on_void_added, event)
-    end
-
-    local created_entities = {}
-
-    for _, entity in pairs(entities) do
-        created_entity = surface.create_entity(entity)
-        if (nil == created_entity) then
-            error('Failed creating entity ' .. entity.name .. ' on surface.')
-        end
-
-        if ('sand-rock-big' == created_entity.name) then
-            created_entity.destructible = false
-        end
-
-        table.insert(created_entities, created_entity)
-    end
-
-    for _, entity in pairs(created_entities) do
-        script.raise_event(Template.events.on_placed_entity, {entity = entity})
-    end
+    Task.queue_task(insert_token, data, total_calls)
 end
 
 return Template
