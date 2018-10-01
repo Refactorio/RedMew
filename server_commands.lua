fff6559f2eb6bc131980bf61b6d8a97c0518f147
grilledham <jrdgillham@gmail.com>
2018-10-02 00:10:53 +0100

    updates

--- server_commands.lua
+++ server_commands.lua
@@ -1,9 +1,22 @@
 local Poll = require 'poll'
+local UserGroups = require 'user_groups'
 
 local Public = {}
 
 function Public.get_poll_result(id)
     Poll.send_poll_result_to_discord(id)
+end
+
+function Public.regular_sync(names)
+    global.regulars = names
+end
+
+function Public.regular_promote(name)
+    global.regulars[name] = true
+end
+
+function Public.regular.demote(name)
+    global.regulars[name] = nil
 end
 
 return Public
