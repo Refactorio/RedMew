3d6f5723bc81fa906f15ccfdbd0139fab0e402a5
Matthew <plague006@users.noreply.github.com>
2018-11-12 12:27:04 -0500

    Move map info to config

--- config.lua
+++ config.lua
@@ -20,3 +20,12 @@ global.scenario.custom_functions = {}
 global.scenario.config.nuke_control.nuke_min_time_hours = 3 --how long a player must be on the server to be allowed to use the nuke
 global.newline = '\n'
 newline = '\n'
+
+-- The title of the map
+global.scenario.config.map_name_key = 'This Map has no name'
+-- The long description of the map, typically 1 paragraph
+global.scenario.config.map_description_key = "By default this section is blank as it's supposed to be filled out on a per map basis. (If you're seeing this message, ping the admin team to get a description added for this map)"
+-- The feature list of the map
+global.scenario.config.map_extra_info_key = 'This map has no extra infomation'
+-- New Scenario Features, appears in the "What's new" tab
+global.scenario.config.new_info_key = 'Nothing is new. The world is at peace'
