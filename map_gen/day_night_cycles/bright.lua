654216f14904c17749a2c9c43d7347318c6022cd
Matthew <plague006@users.noreply.github.com>
2018-11-18 07:00:22 -0500

    Add day/night cycle options (#360)
    * Add day/night cycle options
    
    * txt to markdown and fix formatting errors

---
+++ map_gen/day_night_cycles/bright.lua
@@ -0,0 +1,6 @@
+-- 10 minute cycle, 4m of full light, 4m light to dark, 6s full dark, 2m dark to light
+game.player.surface.ticks_per_day = 36000
+game.player.surface.dusk = 0.2
+game.player.surface.evening = 0.59
+game.player.surface.morning = 0.6
+game.player.surface.dawn = 0.8
---
+++ map_gen/day_night_cycles/day_night_cycle.lua
@@ -0,0 +1,11 @@
+local Event = require "utils.event"
+
+-- Uncomment the desired day/night cycle
+
+-- requires
+-- requires
+
+local function init()
+end
+
+Event.on_init(init)
