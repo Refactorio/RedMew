4dea596b12e1b1bdaf0082b95c9c5633f5604e3e
plague006 <plague006@gmail.com>
2018-11-29 13:54:50 -0500

    Add new stat tracking

--- features/player_stats.lua
+++ features/player_stats.lua
@@ -1,13 +1,29 @@
 local Event = require 'utils.event'
 local Global = require 'utils.global'
 local Game = require 'utils.game'
+require 'utils.table'
 
 local player_last_position = {}
 local player_walk_distances = {}
 local player_coin_earned = {}
 local player_coin_spent = {}
+local player_crafted_items = {}
+local player_console_chats = {}
+local player_damage_taken = {}
+local player_damage_dealt = {}
 local player_deaths = {}
 local total_players = {0}
+local total_train_kills = {0}
+local total_trees_mined = {0}
+local total_rocks_mined = {0}
+local total_robot_built_entities = {0}
+
+local train_kill_causes = {
+    'locomotive',
+    'cargo-wagon',
+    'fluid-wagon',
+    'artillery-wagon'
+}
 
 Global.register(
     {
@@ -16,7 +32,15 @@ Global.register(
         player_coin_earned = player_coin_earned,
         player_coin_spent = player_coin_spent,
         player_deaths = player_deaths,
-        total_players = total_players
+        total_players = total_players,
+        total_train_kills = total_train_kills,
+        total_trees_mined = total_trees_mined,
+        total_rocks_mined = total_rocks_mined,
+        player_crafted_items = player_crafted_items,
+        player_console_chats = player_console_chats,
+        player_damage_taken = player_damage_taken,
+        player_damage_dealt = player_damage_dealt,
+        total_robot_built_entities = total_robot_built_entities,
     },
     function(tbl)
         player_last_position = tbl.player_last_position
@@ -25,6 +49,14 @@ Global.register(
         player_coin_spent = tbl.player_coin_spent
         player_deaths = tbl.player_deaths
         total_players = tbl.total_players
+        total_train_kills = tbl.total_train_kills
+        total_trees_mined = tbl.total_trees_mined
+        total_rocks_mined = tbl.total_rocks_mined
+        player_crafted_items = tbl.player_crafted_items
+        player_console_chats = tbl.player_console_chats
+        player_damage_taken = tbl.player_damage_taken
+        player_damage_dealt = tbl.player_damage_dealt
+        total_robot_built_entities = tbl.total_robot_built_entities
     end
 )
 
@@ -35,6 +67,9 @@ local function player_created(event)
     player_walk_distances[index] = 0
     player_coin_earned[index] = 0
     player_coin_spent[index] = 0
+    player_crafted_items[index] = 0
+    player_damage_taken[index] = 0
+    player_damage_dealt[index] = 0
     player_deaths[index] = {causes = {}, count = 0}
     total_players[1] = total_players[1] + 1
 end
@@ -65,6 +100,10 @@ local function player_died(event)
     local causes = data.causes
     local cause_count = causes[cause] or 0
     causes[cause] = cause_count + 1
+
+    if table.contains(train_kill_causes, cause) then
+        total_train_kills = total_train_kills + 1
+    end
 end
 
 local function picked_up_item(event)
@@ -74,6 +113,44 @@ local function picked_up_item(event)
         local player_index = event.player_index
         player_coin_earned[player_index] = player_coin_earned[player_index] + stack.count
     end
+end
+
+local function player_mined_item(event)
+    if event.entity.type == 'simple-entity' then -- Cheap check for rock, may have other side effects
+        total_rocks_mined[1] = total_rocks_mined[1] + 1
+        return
+    end
+    if event.entity.type == 'tree' then
+        total_trees_mined[1] = total_trees_mined[1] + 1
+    end
+end
+
+local function player_crafted_item(event)
+    local stack = event.item_stack
+    local player_index = event.player_index
+    player_crafted_items[player_index] = player_crafted_items[player_index] + stack.count
+end
+
+local function player_console_chat(event)
+    local player_index = event.player_index
+    if player_index then
+        player_console_chats[player_index] = player_console_chats[player_index] + 1
+    end
+end
+
+local function entity_damaged(event)
+    if event.entity.type == 'player' then -- player taking damage
+        local index = event.entity.player.index
+        player_damage_taken[index] = player_damage_taken[index] + event.final_damage_amount
+    end
+    if event.cause.type == 'player' then -- player causing damage
+        local index = event.cause.player.index
+        player_damage_dealt[index] = player_damage_dealt[index] + event.final_damage_amount
+    end
+end
+
+local function robot_built_entity()
+    total_robot_built_entities[1] = total_robot_built_entities[1] + 1
 end
 
 local function tick()
@@ -95,6 +172,12 @@ end
 Event.add(defines.events.on_player_created, player_created)
 Event.add(defines.events.on_player_died, player_died)
 Event.add(defines.events.on_picked_up_item, picked_up_item)
+Event.add(defines.events.on_pre_player_mined_item, player_mined_item)
+Event.add(defines.events.on_player_crafted_item, player_crafted_item)
+Event.add(defines.events.on_console_chat, player_console_chat)
+Event.add(defines.events.on_entity_damaged, entity_damaged)
+Event.add(defines.events.on_robot_built_entity, robot_built_entity)
+
 Event.on_nth_tick(62, tick)
 
 local Public = {}
@@ -131,13 +214,45 @@ function Public.get_death_count(player_index)
     return player_deaths[player_index].count
 end
 
--- Returns a dictionary of casue_name -> count
+-- Returns a dictionary of cause_name -> count
 function Public.get_all_death_counts_by_casue(player_index)
     return player_deaths[player_index].causes or {}
 end
 
 function Public.get_total_player_count()
     return total_players[1]
+end
+
+function Public.get_total_train_kills()
+    return total_train_kills[1]
+end
+
+function Public.get_total_trees_mined()
+    return total_trees_mined[1]
+end
+
+function Public.get_total_rocks_mined()
+    return total_rocks_mined[1]
+end
+
+function Public.get_player_crafted_item(player_index)
+    return player_crafted_items[player_index]
+end
+
+function Public.get_player_console_chat(player_index)
+    return player_console_chats[player_index]
+end
+
+function Public.get_player_damage_taken(player_index)
+    return player_damage_taken[player_index]
+end
+
+function Public.get_player_damage_dealt(player_index)
+    return player_damage_dealt[player_index]
+end
+
+function Public.get_total_robot_built_entities()
+    return total_robot_built_entities[1]
 end
 
 return Public
