3c861b86fc94d351713bb08d7799bac19e1203df
Matthew <plague006@users.noreply.github.com>
2018-12-03 11:49:24 -0500

    Ensure donators are always regulars

--- features/user_groups.lua
+++ features/user_groups.lua
@@ -12,7 +12,7 @@ local Module = {}
 
 Module.is_regular =
     function(player_name)
-    return Utils.cast_bool(global.regulars[player_name] or global.regulars[player_name:lower()]) --to make it backwards compatible
+    return Utils.cast_bool(global.regulars[player_name] or global.regulars[player_name:lower()]) or global.Donators[player_name] --to make it backwards compatible
 end
 
 Module.add_regular = function(player_name)
