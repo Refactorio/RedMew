8c2614f5c6e711288d9502dc1c0a4c448a85cc3d
plague006 <plague006@gmail.com>
2018-12-06 13:22:02 -0500

    Move where to find names higher up

--- map_selection.lua
+++ map_selection.lua
@@ -2,6 +2,7 @@ local map_name = 'default'
 
 --[[
 Replace the word default in the quotes above with the name of the map you want to play then save this file.
+You can get the full list of map names by looking in map_names.lua
 Names of some popular maps:
 diggy
 crash_site
@@ -30,7 +31,6 @@ If you want to play beach, line 1 should look like:
 local map_name = 'beach'
 
 If default is left in place, you will get a vanilla world.
-You can get the full list of map names by looking in map_names.lua
 You can also use 'map_layout' to load that file and create custom layouts.
 ]]
 return map_name
