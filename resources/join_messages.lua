96fa6990de9830de04e0189b3f5fe88698202dea
Matthew <plague006@users.noreply.github.com>
2018-12-11 15:40:06 -0500

    Remove "Press F to pay respect and loot their items" from join_messages

--- resources/join_messages.lua
+++ resources/join_messages.lua
@@ -2,7 +2,6 @@
 return {
     {'And remember.. Keep Calm And Spaghetti!', 50},
     {'Not sure what to do? Ask Newcott.', 1},
-    {'Press F to pay respects and loot their items', 10},
     {'Stick and move, stick and move', 10},
     {'Fly like a butterfly, sting like a bee', 10},
     {"These are not the bots you're looking for", 10}
