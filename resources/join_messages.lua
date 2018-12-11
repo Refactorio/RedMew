1942f0cf0fb435e577e5941e2ed6c57bbe5125c9
Matthew <plague006@users.noreply.github.com>
2018-12-11 15:50:37 -0500

    Add "Press F to pay respects to the "Press F to pay respect" message" to join_messages

--- resources/join_messages.lua
+++ resources/join_messages.lua
@@ -1,6 +1,7 @@
 -- List of join messages and their weights. Default is 10.
 return {
     {'And remember.. Keep Calm And Spaghetti!', 50},
+    {'Press F to pay respects to the "Press F to pay respect" message', 3},
     {'Not sure what to do? Ask Newcott.', 1},
     {'Stick and move, stick and move', 10},
     {'Fly like a butterfly, sting like a bee', 10},
