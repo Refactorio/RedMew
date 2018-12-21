96b4df23307f4380771265e128812c2c649a2ef9
plague006 <plague006@gmail.com>
2018-12-21 18:55:10 -0500

    Step - reduce dump_env

--- utils/dump_env.lua
+++ utils/dump_env.lua
@@ -1,23 +1,18 @@
--- A small debugging tool that writes the contents of _ENV to a file when the game loads.
--- Useful for ensuring you get the same information when loading
--- the reference and desync levels in desync reports.
 local inspect = require 'inspect'
 local Event = require 'utils.event'
-local filename = 'env_dump.lua'
 
-local remove_package = function(item)
-	if item ~= 'package' then return item end
-  end
+local filter = function(item)
+    if item ~= 'whatever' then
+        return item
+    end
+end
 
-local function player_joined(event)
-	local dump_string = inspect(global, {process = remove_package})
-	if dump_string then
-		local s = string.format('tick on join: %s\n%s', event.tick, dump_string)
-		game.write_file(filename, s)
-		game.print('_ENV dumped into ' .. filename)
-	else
-		game.print('_ENV not dumped, dump_string was nil')
-	end
+local function player_joined()
+    local dump_string = inspect(global, {process = filter})
+    if dump_string then
+        game.write_file('dump.lua', dump_string)
+        game.print('dumped')
+    end
 end
 
 Event.add(defines.events.on_player_joined_game, player_joined)
