26180d36a1a839cd346d0f189135052f5da8b55e
Valansch <mail@maikwild.de>
2018-12-22 00:35:02 +0100

    surfing 8-)

--- map_gen/combined/tetris/tetrimino.lua
+++ map_gen/combined/tetris/tetrimino.lua
@@ -4,6 +4,7 @@ local Token = require 'utils.token'
 local Task = require 'utils.schedule'
 local Queue = require 'utils.q'
 local Global = require 'utils.global'
+local Game = require 'utils.game'
 
 local insert = table.insert
 
@@ -36,7 +37,7 @@ local collision_boxes = {
         {0, 0, 0, 0},
         {1, 1, 0, 0},
         {0, 1, 1, 0},
-        {0, 0, 0, 0}, 
+        {0, 0, 0, 0},
     },
     {
         {0, 0, 0, 0},
@@ -71,18 +72,18 @@ local function collides(self, collision_box, x_steps, y_steps)
     local c_x = position.x
     local c_y = position.y
     for y = 1, 4 do
-        for x = 1, 4 do 
+        for x = 1, 4 do
             local bit = collision_box[y][x]
             if bit == 1 then
                 local y_offset = y + y_steps
                 local x_offset = x + x_steps
-                if  
+                if
                     y_offset < 1 or --Cant collide with itself, so continue checking for collision
                     y_offset > 4 or
                     x_offset < 1 or
                     x_offset > 4 or
                     old_collision_box[y_offset][x_offset] == 0 --check for collision if not colliding with old self
-                then 
+                then
                     local x_target = x_offset * 16 + c_x + 2 - 16
                     local y_target = y_offset * 16 + c_y + 2 - 16
                     local tile = surface.get_tile{x_target, y_target}
@@ -98,44 +99,54 @@ end
 
 local function erase_qchunk(surface, x, y)
     local new_tiles = {}
-    for c_x = x + 1 , x + 14 do 
-        for c_y = y + 1, y + 14 do 
+    for c_x = x + 1 , x + 14 do
+        for c_y = y + 1, y + 14 do
             insert(new_tiles, {name = "water", position = {c_x, c_y}})
         end
     end
     local y_plus15 = y + 15
-    for c_x = x, x + 15 do 
+    for c_x = x, x + 15 do
         insert(new_tiles, {name = "deepwater", position = {c_x, y}})
         insert(new_tiles, {name = "deepwater", position = {c_x, y_plus15}})
     end
 
     local x_plus15 = x + 15
-    for c_y = y, y + 14 do 
+    for c_y = y, y + 14 do
         insert(new_tiles, {name = "deepwater", position = {x, c_y}})
         insert(new_tiles, {name = "deepwater", position = {x_plus15, c_y}})
     end
     surface.set_tiles(new_tiles)
 end
 
-function move_qchunk(surface, x, y, x_offset, y_offset) 
+function move_qchunk(surface, x, y, x_offset, y_offset)
     local entities = surface.find_entities_filtered{area = {{x,y}, {x + 15, y + 15}}}
     local old_tiles = surface.find_tiles_filtered{area = {{x,y}, {x + 16, y + 16}}}
     local new_tiles = {}
-    for index, tile in ipairs(old_tiles) do 
+    local player_positions = {}
+    for index, tile in ipairs(old_tiles) do
         local old_pos = tile.position
         new_tiles[index] = {name = tile.name, position = {x = old_pos.x + x_offset, y = old_pos.y + y_offset}}
     end
 
-    for index,entity in ipairs(entities) do 
+    for index, entity in ipairs(entities) do
         local old_pos = entity.position
         local amount = nil
-        if entity.type == "resource" then 
+        if entity.type == "resource" then
             amount = entity.amount
         end
-        surface.create_entity{force=entity.force, amount = amount, name = entity.name, position = {old_pos.x + x_offset, old_pos.y + y_offset}}
+        local type = entity.type
+        if type ~= 'player' and type ~= 'character-corpse' then
+            surface.create_entity{force=entity.force, amount = amount, name = entity.name, position = {old_pos.x + x_offset, old_pos.y + y_offset}}
+        elseif entity.type == 'player' and entity.player then
+            player_positions[entity.player.index] = {old_pos.x + x_offset, old_pos.y + y_offset}
+            entity.player.teleport{0, 0}
+        end
     end
     surface.set_tiles(new_tiles)
     erase_qchunk(surface, x, y)
+    for player_index, position in pairs(player_positions) do
+        Game.get_player_by_index(player_index).teleport(position)
+    end
 end
 
 function Module.move(self, x_direction, y_direction)
@@ -147,7 +158,7 @@ function Module.move(self, x_direction, y_direction)
     local tetri_y = position.y
     if y_direction == 1 then
         for y = 4, 1, -1 do
-            for x = 1, 4 do 
+            for x = 1, 4 do
                 if collision_box[y] and collision_box[y][x] == 1 then
                     Queue.push(move_queue, {surface = surface, x = tetri_x + (x - 1) * 16, y = tetri_y + (y - 1) * 16 , x_offset = 0, y_offset = 16})
                 end
@@ -172,14 +183,14 @@ function rotate_collision_box(collision_box, reverse)
     local new_collision_box = {{},{},{},{}}
     local transformation = {{},{},{},{}}
     if reverse then
-        for y = 1, 4 do 
+        for y = 1, 4 do
             for x = 1, 4 do
                 new_collision_box[y][x] = collision_box[5 - x][y]
                 transformation[5 - x][y] = {x = x, y = y}
             end
         end
     else
-        for y = 1, 4 do 
+        for y = 1, 4 do
             for x = 1, 4 do
                 new_collision_box[y][x] = collision_box[x][5 - y]
                 transformation[x][5 - y] = {x = x, y = y}
@@ -220,10 +231,10 @@ function Module.rotate(self, reverse)
     local tiles = {}
     local entities = {}
 
-    for x = 1, 4 do 
-        for y = 1, 4 do 
+    for x = 1, 4 do
+        for y = 1, 4 do
             local target = transformation[y][x]
-            if 
+            if
                 (target.x ~= x or target.y ~= y) and --Do not rotate identity
                 old_collision_box[y][x] == 1 --check for existence
             then
@@ -231,16 +242,27 @@ function Module.rotate(self, reverse)
                 local top_left_y = tetri_y + y * 16 - 16
                 local area = {{top_left_x, top_left_y},{tetri_x + x * 16 - 1, tetri_y + y * 16 - 1}}
 
-                for _, tile in pairs(find_tiles_filtered{area = {{top_left_x, top_left_y},{tetri_x + x * 16, tetri_y + y * 16}}}) do 
+                for _, tile in pairs(find_tiles_filtered{area = {{top_left_x, top_left_y},{tetri_x + x * 16, tetri_y + y * 16}}}) do
                     insert(tiles, {name = tile.name, position = {tile.position.x + (target.x - x) * 16, tile.position.y + (target.y - y) * 16}})
                 end
 
-                for _, entity in pairs(find_entities_filtered{area = {{top_left_x, top_left_y},{tetri_x + x * 16 - 1, tetri_y + y * 16 - 1}}}) do 
-                    insert(entities, {name = entity.name, position = {entity.position.x + (target.x - x) * 16, entity.position.y + (target.y - y) * 16}})
+                for _, entity in pairs(find_entities_filtered{area = {{top_left_x, top_left_y},{tetri_x + x * 16 - 1, tetri_y + y * 16 - 1}}}) do
+                    local type = entity.type
+                    if type ~= 'player' and type ~= 'character-corpse' then
+                        insert(entities, {name = entity.name, position = {entity.position.x + (target.x - x) * 16, entity.position.y + (target.y - y) * 16}})
+                    elseif entity.type == 'player' then
+                        entity.player.teleport{entity.position.x + (target.x - x) * 16, entity.position.y + (target.y - y) * 16}
+                    end
                 end
-                erase_qchunk(surface, top_left_x, top_left_y)
-            end 
+                if new_collision_box[y][x] == 0 then
+                    erase_qchunk(surface, top_left_x, top_left_y)
+                end
+            end
         end
+    end
+
+    for _, e in pairs(entities) do
+        surface.create_entity{position = e.position, name = e.name}
     end
 
     surface.set_tiles(tiles)
