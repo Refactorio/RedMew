1344fd1df6ce16057d5beeaf9ca1d0edd2bc793b
plague006 <plague006@gmail.com>
2018-12-21 20:42:06 -0500

    No crash because tokens is filtered

--- control.lua
+++ control.lua
@@ -5,13 +5,13 @@ local inspect = require 'inspect'
 local Event = require 'utils.event'
 
 local filter = function(item)
-    if item ~= 'whatever' then
+    if item ~= 'tokens' then
         return item
     end
 end
 
 local function player_joined()
-    local dump_string = inspect(global.tokens, {process = filter})
+    local dump_string = inspect(global, {process = filter})
     if dump_string then
         game.write_file('dump.lua', dump_string)
         game.print('dumped')
