df0866515fda363fc9841c4242c682611b601d6b
plague006 <plague006@gmail.com>
2018-12-21 19:10:50 -0500

    Put inspect into control

--- control.lua
+++ control.lua
@@ -3,6 +3,24 @@ _DUMP_ENV = true
 --require 'features.gui.info'
 require 'features.gui.paint'
 
-if _DUMP_ENV then
-    require 'utils.dump_env'
+
+
+-- Dump code
+local inspect = require 'inspect'
+local Event = require 'utils.event'
+
+local filter = function(item)
+    if item ~= 'whatever' then
+        return item
+    end
 end
+
+local function player_joined()
+    local dump_string = inspect(global, {process = filter})
+    if dump_string then
+        game.write_file('dump.lua', dump_string)
+        game.print('dumped')
+    end
+end
+
+Event.add(defines.events.on_player_joined_game, player_joined)
