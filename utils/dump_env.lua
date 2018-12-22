4a8ab3e1beae775d4da2980d30c1b99f8986735e
plague006 <plague006@gmail.com>
2018-12-21 21:12:22 -0500

    Fix metatable crash

--- utils/dump_env.lua
+++ utils/dump_env.lua
@@ -4,20 +4,24 @@
 require 'utils.table'
 local Event = require 'utils.event'
 local filename = 'env_dump.lua'
+local inspect = table.inspect
 
-local remove_package = function(item)
-	if item ~= 'package' then return item end
+-- Removes metatables and the package table
+local filter = function(item, path)
+    if path[#path] ~= inspect.METATABLE and item ~= 'package' then
+        return item
+    end
 end
 
 local function player_joined(event)
-	local dump_string = table.inspect(_ENV, {process = remove_package})
-	if dump_string then
-		local s = string.format('tick on join: %s\n%s', event.tick, dump_string)
-		game.write_file(filename, s)
-		game.print('_ENV dumped into ' .. filename)
-	else
-		game.print('_ENV not dumped, dump_string was nil')
-	end
+    local dump_string = inspect(_ENV, {process = filter})
+    if dump_string then
+        local s = string.format('tick on join: %s\n%s', event.tick, dump_string)
+        game.write_file(filename, s)
+        game.print('_ENV dumped into ' .. filename)
+    else
+        game.print('_ENV not dumped, dump_string was nil')
+    end
 end
 
 Event.add(defines.events.on_player_joined_game, player_joined)
