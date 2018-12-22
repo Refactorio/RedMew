2c2e3a31db518d3e177ee788f9cec54ce9d1eaa2
plague006 <plague006@gmail.com>
2018-12-21 19:37:39 -0500

    Further reduce paint

--- features/gui/paint.lua
+++ features/gui/paint.lua
@@ -2,26 +2,12 @@ local Event = require 'utils.event'
 local Gui = require 'utils.gui'
 local Game = require 'utils.game'
 
-local main_button_name = Gui.uid_name()
 local main_frame_name = Gui.uid_name()
 
 local filter_button_name = Gui.uid_name()
 local filter_clear_name = Gui.uid_name()
 
 global.paint_brushes_by_player = {}
-
-local function player_joined(event)
-    local player = Game.get_player_by_index(event.player_index)
-    if not player or not player.valid then
-        return
-    end
-
-    if player.gui.top[main_button_name] ~= nil then
-        return
-    end
-
-    player.gui.top.add {name = main_button_name, type = 'sprite-button', sprite = 'utility/spray_icon'}
-end
 
 local function toggle(event)
     local left = event.player.gui.left
@@ -37,11 +23,6 @@ local function toggle(event)
             direction = 'vertical',
             caption = 'Paint Brush'
         }
-        main_frame.add {
-            type = 'label',
-            caption = 'Choose a replacement tile for Refined hazard concrete'
-        }
-
         local brush =
             main_frame.add({type = 'flow'}).add {
             type = 'sprite-button',
@@ -52,8 +33,6 @@ local function toggle(event)
         Gui.set_data(clear_bursh, brush)
     end
 end
-
-Event.add(defines.events.on_player_joined_game, player_joined)
 
 local function player_created(event)
     local player = Game.get_player_by_index(event.player_index)
