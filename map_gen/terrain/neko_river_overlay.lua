d3b57899ab855c38b208106ffbdf6834f8741cb5
grilledham <jrdgillham@gmail.com>
2019-01-27 12:22:24 -0500

    Grilled brings neko's modules up to date

--- map_gen/terrain/neko_river_overlay.lua
+++ map_gen/terrain/neko_river_overlay.lua
@@ -1,20 +1,25 @@
 local perlin = require 'map_gen.shared.perlin_noise'
-local Event = require 'utils.event'
+local Global = require 'utils.global'
 
-local function init()
-    global.terrain_seed_A = math.random(10, 10000)
-    global.terrain_seed_B = math.random(10, 10000)
-end
+local seed
 
-Event.on_init(init)
+Global.register_init(
+    {},
+    function(tbl)
+        tbl.seed = math.random(10, 10000)
+    end,
+    function(tbl)
+        seed = tbl.seed
+    end
+)
 
 return function(x, y)
-    local wiggle = 50 + perlin.noise((x * 0.005), (y * 0.005), global.terrain_seed_A + 71) * 60
-    local terrain_A = perlin.noise((x * 0.005), (y * 0.005), global.terrain_seed_A + 19) * wiggle --For determining where water is
+    local wiggle = 50 + perlin.noise((x * 0.005), (y * 0.005), seed + 71) * 60
+    local terrain_A = perlin.noise((x * 0.005), (y * 0.005), seed + 19) * wiggle --For determining where water is
     local terrain_sqr = terrain_A * terrain_A --we can use this again to mess with other layers as well
 
     if terrain_sqr < 50 then --Main water areas
-        terrain_A = perlin.noise((x * 0.01), (y * 0.01), global.terrain_seed_A + 31) * 90 + (wiggle * -0.2) --we only gen this when we consider placing water
+        terrain_A = perlin.noise((x * 0.01), (y * 0.01), seed + 31) * 90 + (wiggle * -0.2) --we only gen this when we consider placing water
 
         if terrain_A * terrain_A > 40 then --creates random bridges over the water by overlapping with another noise layer
             return 'water'
