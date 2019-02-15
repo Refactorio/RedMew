23121cd31d2300ab3686134be647e7a8fdcb6f54
Matthew Heguy <plague006@gmail.com>
2019-02-14 19:02:39 -0500

    Fix desync apoc: set data in token rather than relying on passed data to be valid hydra: move data into Global to allow changes during runtime

--- features/apocalypse.lua
+++ features/apocalypse.lua
@@ -1,4 +1,3 @@
-local table = require 'utils.table'
 local Task = require 'utils.task'
 local Token = require 'utils.token'
 local Game = require 'utils.game'
@@ -6,21 +5,29 @@ local Toast = require 'features.gui.toast'
 local RS = require 'map_gen.shared.redmew_surface'
 local HailHydra = require 'map_gen.shared.hail_hydra'
 
-local clear_table = table.clear_table
-
 local Public = {}
 
-local hydra_config = {
+local hail_hydra_data = {
     ['behemoth-spitter'] = {['behemoth-spitter'] = 0.01},
     ['behemoth-biter'] = {['behemoth-biter'] = 0.01}
 }
 
 local biter_spawn_token =
     Token.register(
-    function(data)
-        local surface = data.surface
-        local group = data.group
-        local p_spawn = data.p_spawn
+    function()
+        local surface
+        local player_force
+        local enemy_force = game.forces.enemy
+
+        surface = RS.get_surface()
+        player_force = game.forces.player
+
+        HailHydra.set_hydras(hail_hydra_data)
+        HailHydra.enable_hail_hydra()
+        enemy_force.evolution_factor = 1
+
+        local p_spawn = player_force.get_spawn_position(surface)
+        local group = surface.create_unit_group {position = p_spawn}
 
         local create_entity = surface.create_entity
 
@@ -34,7 +41,7 @@ local biter_spawn_token =
         for i = 1, #aliens do
             local spawn_pos = surface.find_non_colliding_position('behemoth-biter', p_spawn, 300, 1)
             if spawn_pos then
-                local biter = create_entity {name = aliens[i], position = spawn_pos}
+                local biter = create_entity({name = aliens[i], position = spawn_pos})
                 group.add_member(biter)
             end
         end
@@ -44,7 +51,7 @@ local biter_spawn_token =
     end
 )
 
-function Public.begin_apocalypse(args, player)
+function Public.begin_apocalypse(args)
     if args.confirmation ~= 'end this map' then
         Game.player_print('You must use /apocalypse end this map')
         return
@@ -55,40 +62,10 @@ function Public.begin_apocalypse(args, player)
     end
     game.server_save('pre-apocalypse')
     global.apocalypse_now = true
-    local surface
-    local player_force
-    local enemy_force = game.forces.enemy
-
-    if player and player.valid then
-        surface = player.surface
-        player_force = player.force
-    else
-        surface = RS.get_surface()
-        player_force = game.forces.player
-    end
-
-    local hydras = global.config.hail_hydra.hydras
-    clear_table(hydras)
-    for k, v in pairs(hydra_config) do
-        hydras[k] = v
-    end
-    HailHydra.enable_hail_hydra()
-    enemy_force.evolution_factor = 1
-
-    local p_spawn = player_force.get_spawn_position(surface)
-    local group = surface.create_unit_group {position = p_spawn}
 
     game.print('The ground begins to rumble. It seems as if the world itself is coming to an end.')
 
-    Task.set_timeout(
-        60,
-        biter_spawn_token,
-        {
-            p_spawn = p_spawn,
-            group = group,
-            surface = surface
-        }
-    )
+    Task.set_timeout(60, biter_spawn_token, {})
 end
 
 return Public
