acfb84d50d634f6790ed7904b4c87c55be32c228
Valansch <Mail@maikwild.de>
2019-02-17 12:57:08 +0100

    Fixed missing imports

--- map_gen/shared/map_loader.lua
+++ map_gen/shared/map_loader.lua
@@ -2,6 +2,11 @@ local success, shape = pcall(function() require 'map_selection' end)
 if not success then
 	error('\n NO MAP SELECTED \n The RedMew scenario contains many different maps. If you are unsure how to select a map checkout our guide at: \n https://redmew.com/guide \n -The RedMew Team ', 0)
 end
+
+local b = require 'map_gen.shared.builders'
+local RS = require 'map_gen.shared.redmew_surface'
+local config = global.config.map_generation
+
 if #config.entity_modules > 0 then
     shape = shape or b.full_shape
     shape = b.apply_entities(shape, config.entity_modules)
