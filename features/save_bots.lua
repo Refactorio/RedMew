8c71a7c6856044d36c5900430d5bc939fc0da0a5
SimonFlapse <signup@simonflarup.dk>
2019-02-22 13:32:24 +0100

    Added aditional checks to prevent indexing nil

--- features/save_bots.lua
+++ features/save_bots.lua
@@ -9,22 +9,33 @@ local function preserve_bot(event)
         return
     end
 
-    if entity.name == 'construction-robot' then
-        local logistic_cell = entity.logistic_network.cells[1]
-
-        --Checks if construction-robot is part of a mobile logistic network
-        if logistic_cell.owner.name ~= 'player' then
-            return
-        end
-
-        --checks if construction-robot is owned by the player that has selected it
-        if logistic_cell.owner.player.name == player.name then
-            entity.minable = true
-            return
-        end
-
-        entity.minable = false
+    if entity.name ~= 'construction-robot' then
+        return
     end
+    local logistic_network = entity.logistic_network
+
+    if logistic_network == nil or not logistic_network.valid then
+        --prevents an orphan bot from being unremovable
+        entity.minable = false
+        return
+    end
+
+    -- All valid logistic networks should have at least one cell
+    local cell = logistic_network.cells[1]
+    local owner = cell.owner
+
+    --checks if construction-robot is part of a mobile logistic network
+    if owner.name ~= 'player' then
+        return
+    end
+
+    --checks if construction-robot is owned by the player that has selected it
+    if owner.player.name == player.name then
+        entity.minable = true
+        return
+    end
+
+    entity.minable = false
 end
 
 Event.add(defines.events.on_selected_entity_changed, preserve_bot)
