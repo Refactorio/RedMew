2caa46161e0122a5de290afb90a7570e21c665a8
Quadrum1 <38959426+Quadrum1@users.noreply.github.com>
2019-02-23 20:36:17 +0100

    Update tag_groups.lua

--- resources/tag_groups.lua
+++ resources/tag_groups.lua
@@ -5,75 +5,75 @@ return {
     ['Trooper'] = {
         path = 'item/tank',
         verb = 'strengthened',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
 		
     },
     ['Mining'] = {
         path = 'item/electric-mining-drill',
         verb = 'enriched',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Smelting'] = {
         path = 'item/stone-furnace',
         verb = 'fused',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"		
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"		
     },
     ['Production'] = {
         path = 'item/assembling-machine-2',
         verb = 'enhanced',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
 		
     },
     ['Science'] = {
         path = 'item/science-pack-3',
         verb = 'advanced',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Wizard'] = {
         path = 'item/green-wire',
         verb = 'combinated',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Trains'] = {
         path = 'item/locomotive',
         verb = 'derailed',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Oil'] = {
         path = 'fluid/crude-oil',
         verb = 'lubricated',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Powah!'] = {
         path = 'item/steam-engine',
         verb = 'electrified',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Spaceman'] = {
         path = 'item/rocket-silo',
         verb = 'warped',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Cat'] = {
         path = 'item/raw-fish',
         verb = 'mewed',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     },
     ['Dog'] = {
         path = 'entity/medium-biter',
         verb = 'woofed',
-		join = "{tag} squad expanded with {player}",
-		leave = "{player} has left the {tag} squad"
+	join = "{tag} squad expanded with {player}",
+	leave = "{player} has left the {tag} squad"
     }
 }
