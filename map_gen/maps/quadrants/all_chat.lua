74e8e587461054ec56440c495b2c31fc52cc32ee
SimonFlapse <signup@simonflarup.dk>
2019-03-02 15:59:18 +0100

    Colors and server protect

--- map_gen/maps/quadrants/all_chat.lua
+++ map_gen/maps/quadrants/all_chat.lua
@@ -2,12 +2,15 @@ local Event = require 'utils.event'
 local Game = require 'utils.game'
 
 local function console_chat(event)
+    if not event.player_index or event.player_index == nil then
+        return
+    end
     local player = Game.get_player_by_index(event.player_index)
     local player_force = player.force
     for _, force in pairs(game.forces) do
         if (string.find(force.name, 'quadrant')) ~= nil then
             if force.name ~= player_force.name then
-                force.print(player.name .. ' ' .. player.tag .. ': ' .. event.message)
+                force.print(player.name .. ' ' .. player.tag .. ': ' .. event.message, player.chat_color)
             end
         end
     end
