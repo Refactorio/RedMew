cae917b8557ded5b752e48af1c5350f109d5cbed
theorangeangle <36866626+theorangeangle@users.noreply.github.com>
2019-03-14 23:15:57 -0500

    layed out desired features

--- map_gen/maps/pinguin/icebergs.lua
+++ map_gen/maps/pinguin/icebergs.lua
@@ -1,3 +1,45 @@
 --most features will be time-based, controlled by scenario.lua with one new feature enabled per hour
 --some will be tied to evolution (marathon mode) or on_research_complete (armor switch) and will be enabled from map start
 --golden goose is an exception
+
+
+--turrets have a 90% chance of being on player force when placed
+
+
+--on_entity_mined: 0.01% chance to spawn another ore (every patch becomes a mixed ore patch)
+
+
+--assemblers cannot be crafted in assemblers (manual craft ONLY for that item, all tiers)
+
+
+--crafting underground belt/pipes will no longer give an even number
+
+
+--player color changes each time they send a message
+
+
+--on_research_complete, your armor is switched with a random player
+
+
+--logistic robots have a small change of becoming distractor/destroyer bots when exiting the roboport
+
+
+--cargo wagon becomes fluid wagon and vice versa when a player enters the wagon. it reverts when they exit.
+
+
+--during the night, random entities will take small amounts of damage
+
+
+--player walking direction is reversed at each evolution benchmark (0.1, 0.2, 0.3, etc)
+
+
+--marathon mode enabled at 0.5 evolution
+
+
+--small biters are replaced with compilatrons in hail hydra
+
+
+--all players share the same health bar, proportional to the # of people online
+
+
+--
