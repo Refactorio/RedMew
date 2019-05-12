82874823ce76229265722a2ba01ed3a8f95a135d
TWLTriston <triston@gmail.com>
2019-05-12 19:30:51 -0400

    Fix #910 - added radius lookups for drills

--- features/autodeconstruct.lua
+++ features/autodeconstruct.lua
@@ -4,14 +4,17 @@ local Event = require 'utils.event'
 local Token = require 'utils.token'
 local Task = require 'utils.task'
 
+global.mining_drill_radius_max = 1 -- Defines how far from the resource that depleted we look for a drill
+
 local function is_depleted(drill, entity)
     local position = drill.position
-    local area
-    if drill.name == 'electric-mining-drill' then
-        area = {{position.x - 2.5, position.y - 2.5}, {position.x + 2.5, position.y + 2.5}}
-    else
-        area = {{position.x - 1, position.y - 1}, {position.x + 1, position.y + 1}}
+    local mining_drill_radius = drill.prototype.mining_drill_radius
+
+    if mining_drill_radius == nil then
+      return false
     end
+
+    local area = {{position.x - mining_drill_radius, position.y - mining_drill_radius}, {position.x + mining_drill_radius, position.y + mining_drill_radius}}
 
     for _, resource in pairs(drill.surface.find_entities_filtered {type = 'resource', area = area}) do
         if resource ~= entity and resource.name ~= 'crude-oil' then
@@ -37,7 +40,7 @@ local function on_resource_depleted(event)
     end
 
     local position = entity.position
-    local area = {{position.x - 1, position.y - 1}, {position.x + 1, position.y + 1}}
+    local area = {{position.x - global.mining_drill_radius_max, position.y - global.mining_drill_radius_max}, {position.x + global.mining_drill_radius_max, position.y + global.mining_drill_radius_max}}
     local drills = event.entity.surface.find_entities_filtered {area = area, type = 'mining-drill'}
     for _, drill in ipairs(drills) do
         if drill.name ~= 'pumpjack' and is_depleted(drill, entity) then
@@ -46,4 +49,13 @@ local function on_resource_depleted(event)
     end
 end
 
+local function on_built_entity(event)
+  if event.created_entity.type ~= 'mining-drill' then return end
+  if event.created_entity.prototype.mining_drill_radius > global.mining_drill_radius_max then
+      global.mining_drill_radius_max = event.created_entity.prototype.mining_drill_radius
+  end
+end
+
 Event.add(defines.events.on_resource_depleted, on_resource_depleted)
+Event.add(defines.events.on_robot_built_entity, on_built_entity)
+Event.add(defines.events.on_built_entity, on_built_entity)
