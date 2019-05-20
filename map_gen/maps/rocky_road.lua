bf2382fb4d770a028445916331bd95fb52566ab9
theorangeangle <36866626+theorangeangle@users.noreply.github.com>
2019-05-20 00:12:50 -0500

    added map description info

--- map_gen/maps/rocky_road.lua
+++ map_gen/maps/rocky_road.lua
@@ -7,6 +7,11 @@ local map_entities = {
 local RS = require 'map_gen.shared.redmew_surface'
 local MGSP = require 'resources.map_gen_settings'
 
+ScenarioInfo.set_map_name('Rocky Road')
+ScenarioInfo.set_map_description('We want a vanilla map, they said.\nYou might get the vanilla terrain,\nBut not the vanilla character! Enjoy driving yourself crazy with this one :D')
+ScenarioInfo.add_map_extra_info(
+    'Players stuck in cars, day/night cycle modified to make solar a pain.'
+)
 RS.set_map_gen_settings(
     {
         MGSP.water_none
