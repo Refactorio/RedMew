8440f74244e9307938c644d91342925a1e5940d7
SimonFlapse <signup@simonflarup.dk>
2019-06-08 14:06:14 +0200

    Initial commit of cutscene controller with subtitles
    Included in a WIP scenario that can be used for testing or experimenting.

---
+++ map_gen/maps/factoryfield/cutscene_controller.lua
@@ -0,0 +1,119 @@
+local Event = require 'utils.event'
+local Token = require 'utils.token'
+local Task = require 'utils.task'
+local Global = require 'utils.global'
+local insert = table.insert
+
+local Public = {}
+local handler
+
+local cutscene_functions = {}
+local running_cutscenes = {}
+Global.register(
+    {
+        cutscene_functions = cutscene_functions,
+        running_cutscenes = running_cutscenes
+    },
+    function(tbl)
+        cutscene_functions = tbl.cutscene_functions
+        running_cutscenes = tbl.running_cutscenes
+    end
+)
+
+local function valid(entity)
+    return entity and entity.valid
+end
+
+---Asserts if a given variable is of the expected type using type().
+---
+---@param expected_type string
+---@param given any
+---@param variable_reference_message string displayed when the expectation is not met
+local function assert_type(expected_type, given, variable_reference_message)
+    local given_type = type(given)
+    if given_type ~= expected_type then
+        error('Argument ' .. variable_reference_message .. " must be of type '" .. expected_type .. "', given '" .. given_type .. "'")
+    end
+end
+
+function Public.register_cutscene_function(identifier, waypoints, func)
+    assert_type('string', identifier, 'identifier of function cutscene_controller.register_cutscene_function')
+    assert_type('function', func, 'func of function cutscene_controller.register_cutscene_function')
+    assert_type('table', waypoints, 'waypoints of function cutscene_controller.register_cutscene_function')
+
+    cutscene_functions[identifier] = {func = func, waypoints = waypoints}
+end
+
+function Public.register_running_cutscene(player_index, identifier, final_transition_time)
+    assert_type('number', player_index, 'player_index of function cutscene_controller.register_running_cutscene')
+    assert_type('string', identifier, 'identifier of function cutscene_controller.register_running_cutscene')
+
+    running_cutscenes[player_index] = cutscene_functions[identifier]
+    local player = game.get_player(player_index)
+    if not valid(player) then
+        return
+    end
+    local cutscene_function = cutscene_functions[identifier]
+    if not cutscene_function then
+        return
+    end
+    local waypoints = cutscene_function.waypoints
+    if not waypoints then
+        return
+    end
+    final_transition_time = final_transition_time >= 0 and final_transition_time or 60
+
+    running_cutscenes[player_index]['final_transition_time'] = final_transition_time
+    player.set_controller {
+        type = defines.controllers.cutscene,
+        waypoints = waypoints,
+        final_transition_time = final_transition_time
+    }
+    handler({player_index = player_index, waypoint_index = -1})
+end
+
+local callback_function =
+    Token.register(
+    function(params)
+        params.func(params.player_index, params.waypoint_index, params.params)
+    end
+)
+
+handler = function(event)
+    local player_index = event.player_index
+    local waypoint_index = event.waypoint_index
+
+    local running_cutscene = running_cutscenes[player_index]
+    if not running_cutscene then
+        return
+    end
+
+    local ticks = running_cutscene.waypoints[waypoint_index + 2]
+    if ticks then
+        ticks = ticks.transition_time
+    else
+        ticks = running_cutscene['final_transition_time']
+    end
+
+    local func = running_cutscene.func
+    if not func then
+        return
+    end
+    local current_waypoint = running_cutscene.waypoints[waypoint_index + 2]
+    if not current_waypoint then
+        --game.print('Not current waypoint! Could be last waypoint, defaulting to index one')
+        current_waypoint = running_cutscene.waypoints[1]
+    end
+    local params = {
+        position = current_waypoint.position,
+        time_to_wait = current_waypoint.time_to_wait,
+        transition_time = current_waypoint.transition_time,
+        zoom = current_waypoint.zoom
+    }
+
+    Task.set_timeout_in_ticks(ticks, callback_function, {func = running_cutscene.func, player_index = player_index, waypoint_index = waypoint_index, params = params})
+end
+
+Event.add(defines.events.on_cutscene_waypoint_reached, handler)
+
+return Public
---
+++ map_gen/maps/factoryfield/scenario.lua
@@ -0,0 +1,126 @@
+local b = require 'map_gen.shared.builders'
+local Event = require 'utils.event'
+local Token = require 'utils.token'
+local Task = require 'utils.task'
+local Global = require 'utils.global'
+local CS = require 'map_gen.maps.factoryfield.cutscene_controller'
+local Rendering = require 'map_gen.maps.factoryfield.renderings'
+
+local loading = true
+Global.register(
+    {
+        loading = loading
+    },
+    function(tbl)
+        loading = tbl.loading
+    end
+)
+
+local field = b.circle(512)
+
+local spawn = b.rectangle(32, 32)
+spawn = b.translate(spawn, 0, 544)
+
+local function message(x, y)
+    --if x == -543.5 and y == 639.5 then
+    if x >= 0 and y == 0.5 and loading then
+        --game.print('Done!')
+        game.speed = 1
+        loading = false
+    end
+end
+
+field = b.any({field, spawn, message})
+
+field = b.translate(field, 0, -544)
+
+local sea = b.tile('deepwater')
+local map = b.if_else(field, sea)
+
+local function chart_for_neutral_callback()
+    game.speed = 3
+    game.forces.player.chart(game.surfaces[2], {lefttop = {x = -544, y = -544 * 2}, rightbottom = {x = 512, y = 64}})
+    for _, v in pairs(game.connected_players) do
+        CS.register_running_cutscene(v.index, 'loading', 30)
+    end
+end
+
+local function fading(params)
+    Rendering.blackout(params.player, params.params.zoom, params.delay + 1, {r = 0, g = 0, b = 0, a = params.a})
+end
+
+local fade = Token.register(fading)
+
+local function func(player_index, waypoint_index, params)
+    --game.print('position:' .. serpent.block(params.position) .. ' | trans_time: ' .. params.transition_time .. ' | ttw: ' .. params.time_to_wait .. ' | zoom: ' .. params.zoom)
+
+    if waypoint_index == 0 then
+        local delay = 45
+        local i = 0
+        for a = 1, 0.1, -0.1 do
+            Task.set_timeout_in_ticks(delay * i, fade, {player = game.get_player(player_index), params = params, delay = delay, a = a})
+            i = i + 1
+        end
+    elseif waypoint_index == 1 or waypoint_index == 3 then
+        Rendering.draw_multi_line_text({height = 1440, width = 2560}, 1, params.zoom, {x = 0, y = 18}, {'Hello RedMew!', 'This is a multilined subtitle with more more more text'}, 2.5, params.time_to_wait, game.get_player(player_index), true)
+    elseif waypoint_index == 2 then
+        Rendering.draw_multi_line_text({height = 1440, width = 2560}, 1, params.zoom, {x = 0, y = 18}, {'Hello RedMew!', 'This is subtitles.', 'This is a multilined subtitle with more more more text'}, 2.5, params.time_to_wait, game.get_player(player_index), true)
+    end
+end
+
+local waypoints = {
+    {
+        position = {x = 0, y = 0},
+        transition_time = 0,
+        time_to_wait = 500,
+        zoom = 1
+    },
+    {
+        position = {x = 0, y = 0},
+        transition_time = 0,
+        time_to_wait = 600,
+        zoom = 1
+    },
+    {
+        position = {x = 10, y = 0},
+        transition_time = 60,
+        time_to_wait = 180,
+        zoom = 5
+    },
+    {
+        position = {x = 0, y = 0},
+        transition_time = 180,
+        time_to_wait = 180,
+        zoom = 3
+    },
+    {
+        position = {x = -10, y = 0},
+        transition_time = 120,
+        time_to_wait = 180,
+        zoom = 1
+    }
+}
+
+CS.register_cutscene_function('test', waypoints, func)
+
+local function loading_function(player_index, waypoint_index, params)
+    local player = game.get_player(player_index)
+    if loading then
+        player.jump_to_cutscene_waypoint(0)
+        Rendering.blackout(player, params.zoom, 500 + 60)
+        return
+    end
+    func(player_index, waypoint_index, params)
+end
+
+CS.register_cutscene_function('loading', waypoints, loading_function)
+
+local callback = Token.register(chart_for_neutral_callback)
+
+local function chart_for_neutral()
+    Task.set_timeout_in_ticks(50, callback)
+end
+
+Event.on_init(chart_for_neutral)
+
+return map
