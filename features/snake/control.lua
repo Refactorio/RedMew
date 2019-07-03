ba05138427b8c0066588da14456a197c15bb0c04
grilledham <jrdgillham@gmail.com>
2019-07-03 15:54:06 +0100

    fix typos

--- features/snake/control.lua
+++ features/snake/control.lua
@@ -7,7 +7,7 @@ local Public = {}
 -- Note when players join the game they will lose thier character.
 -- @param surface <LuaSurface> Surface that the board is placed on.
 -- @param top_left_position <Position> Position where board is placed. Defaults to {x = 1, y = 1}.
--- @param size <int> size of board is board tiles. Note that the actaul size fo the board will be 2 * size in factorio tiles. Defaults to 15.
+-- @param size <int> size of board in board tiles. Note that the actual size of the board will be 2 * size in factorio tiles. Defaults to 15.
 -- @param update_rate <int> number of ticks between updates. Defaults to 30.
 -- @param <int> maximun food on the board. Defaults to 6.
 function Public.start_game(surface, top_left_position, size, update_rate, max_food)
