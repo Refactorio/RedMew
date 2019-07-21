20e836c47ea9d6668e430d8bd8a75f9af80fd64b
grilledham <jrdgillham@gmail.com>
2019-07-21 18:37:03 +0100

    outpost balance

--- map_gen/maps/crash_site/outpost_data/big_gear_factory.lua
+++ map_gen/maps/crash_site/outpost_data/big_gear_factory.lua
@@ -9,6 +9,7 @@ local loot = {
     {stack = {name = 'iron-gear-wheel', count = 4000, distance_factor = 2}, weight = 10},
     {stack = {name = 'engine-unit', count = 800, distance_factor = 1 / 2}, weight = 5},
     {stack = {name = 'electric-engine-unit', count = 400, distance_factor = 1 / 2}, weight = 5},
+    {stack = {name = 'flying-robot-frame', count = 400, distance_factor = 1 / 2}, weight = 5},
     {stack = {name = 'rail', count = 2500, distance_factor = 1}, weight = 1},
     {stack = {name = 'tank', count = 5, distance_factor = 1 / 128}, weight = 1},
     {stack = {name = 'locomotive', count = 5, distance_factor = 1 / 128}, weight = 1},
@@ -36,16 +37,16 @@ local factory = {
 local factory_b = {
     callback = ob.magic_item_crafting_callback,
     data = {
-        recipe = 'engine-unit',
-        output = {min_rate = 1 / 60, distance_factor = 1 / 60 / 512, item = 'engine-unit'}
+        recipe = 'flying-robot-frame',
+        output = {min_rate = 1 / 60, distance_factor = 1 / 60 / 512, item = 'flying-robot-frame'}
     }
 }
 
 local factory_c = {
     callback = ob.magic_item_crafting_callback,
     data = {
-        recipe = 'electric-engine-unit',
-        output = {min_rate = 1 / 60, distance_factor = 1 / 60 / 512, item = 'electric-engine-unit'}
+        recipe = 'satellite',
+        output = {min_rate = 0.01 / 60, distance_factor = 0.01 / 60 / 512, item = 'satellite'}
     }
 }
 
@@ -87,6 +88,12 @@ local market = {
             min_price = 0.8
         },
         {
+            name = 'flying-robot-frame',
+            price = 12,
+            distance_factor = 4 / 512,
+            min_price = 1.2
+        },
+        {
             name = 'car',
             price = 50,
             distance_factor = 25 / 512,
@@ -94,9 +101,27 @@ local market = {
         },
         {
             name = 'rail',
-            price = 1,
+            price = 0.5,
             distance_factor = 0.5 / 512,
             min_price = 0.1
+        },
+        {
+            name = 'rail-signal',
+            price = 2,
+            distance_factor = 1 / 512,
+            min_price = 0.1
+        },
+        {
+            name = 'rail-chain-signal',
+            price = 2,
+            distance_factor = 1 / 512,
+            min_price = 0.1
+        },
+        {
+            name = 'train-stop',
+            price = 20,
+            distance_factor = 10 / 512,
+            min_price = 2
         },
         {
             name = 'tank',
@@ -121,6 +146,12 @@ local market = {
             price = 40,
             distance_factor = 20 / 512,
             min_price = 4
+        },
+        {
+            name = 'satellite',
+            price = 300,
+            distance_factor = 50 / 512,
+            min_price = 50
         }
     }
 }
@@ -143,7 +174,7 @@ local level3b =
     {
         factory = factory_b,
         fallback = level2,
-        max_count = 2
+        max_count = 4
     }
 )
 local level3c =
@@ -152,7 +183,7 @@ local level3c =
     {
         factory = factory_c,
         fallback = level3b,
-        max_count = 2
+        max_count = 1
     }
 )
 local level4 =
@@ -168,12 +199,13 @@ return {
         blocks = 9,
         variance = 3,
         min_step = 2,
-        max_level = 2
+        max_level = 3
     },
     walls = {
         require 'map_gen.maps.crash_site.outpost_data.heavy_gun_turrets'
     },
     bases = {
-        {level4, level3, level2}
+        {level3, level2},
+        {level4}
     }
 }
