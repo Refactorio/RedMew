059e8cd274d20d5ad954549a1d6344c8d8ad79a6
SimonFlapse <signup@simonflarup.dk>
2019-10-05 01:01:39 +0200

    Created concrete jungle map
    Suggested by Sangria Louie (https://discordapp.com/channels/432567222481846283/432570869039104012/629791144846426122)
    
    Initial edition
    
    Starting area with a radius of 50 made with stone-path
    
    Most entities can only be placed on top of stone-path or better.
    
    Entities can be split up into three tiers making the "heaviest" entities banned from any tile except refined-concrete
    
    Needs more tiered entities

---
+++ map_gen/maps/concrete_jungle.lua
@@ -0,0 +1,112 @@
+local floor = math.floor
+
+local RestrictEntities = require 'map_gen.shared.entity_placement_restriction'
+local Event = require 'utils.event'
+local b = require 'map_gen.shared.builders'
+
+--- Items explicitly allowed on ores
+RestrictEntities.add_allowed(
+    {
+        'transport-belt',
+        'fast-transport-belt',
+        'express-transport-belt',
+        'underground-belt',
+        'fast-underground-belt',
+        'express-underground-belt',
+        'small-electric-pole',
+        'medium-electric-pole',
+        'big-electric-pole',
+        'substation',
+        'electric-mining-drill',
+        'burner-mining-drill',
+        'pumpjack',
+        'car',
+        'tank',
+        'pumpjack'
+    }
+)
+
+local tiles_tier_1 = {
+    ['stone-path'] = true
+}
+
+local tiles_tier_2 = {
+    ['concrete'] = true,
+    ['hazard-concrete-right'] = true,
+    ['hazard-concrete-left'] = true
+}
+
+local tiles_tier_3 = {
+    ['refined-concrete'] = true,
+    ['refined-hazard-concrete-right'] = true,
+    ['refined-hazard-concrete-left'] = true
+}
+
+
+local tier_2 = {
+    ['oil-refinery'] = true,
+    ['chemical-plant'] = true
+}
+
+local tier_3 = {
+    ['rocket-silo'] = true
+}
+
+--- The logic for checking that there are resources under the entity's position
+RestrictEntities.set_keep_alive_callback(
+    function(entity)
+        local get_tile = entity.surface.get_tile
+        local area = entity.bounding_box
+        local left_top = area.left_top
+        local right_bottom = area.right_bottom
+        local name = entity.name
+
+        if name == 'entity-ghost' then
+            name = entity.ghost_name
+        end
+
+        local is_tier_2 = tier_2[name]
+        local is_tier_3 = tier_3[name]
+
+        for x = floor(left_top.x), floor(right_bottom.x) do
+            for y = floor(left_top.y), floor(right_bottom.y) do
+                local tile_name = get_tile(x, y).name
+                if is_tier_2 then
+                    if not (tiles_tier_2[tile_name] or tiles_tier_3[tile_name]) then
+                        game.print('tier_2')
+                        return false
+                    end
+                elseif is_tier_3 then
+                    if not (tiles_tier_3[tile_name]) then
+                        game.print('tier_3')
+                        return false
+                    end
+                else
+                    if not (tiles_tier_1[tile_name] or tiles_tier_2[tile_name] or tiles_tier_3[tile_name]) then
+                        game.print('tier_1: ' .. tile_name)
+                        return false
+                    end
+                end
+            end
+        end
+        return true
+    end
+)
+
+--- Warning for players when their entities are destroyed
+local function on_destroy(event)
+    local p = event.player
+    if p and p.valid then
+        p.print('You can only build that on top of stone-paths or better. Only belts, mining drills, and power poles are allowed elsewhere.')
+    end
+end
+
+Event.add(RestrictEntities.events.on_restricted_entity_destroyed, on_destroy)
+
+local shape = b.circle(50)
+local stone_path = b.tile('stone-path')
+
+shape = b.invert(shape)
+local map = b.if_else(shape, stone_path)
+
+return map
