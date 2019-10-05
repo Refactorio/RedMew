5d7a3192675573e349826e9b60a9c3b602fce4ff
SimonFlapse <signup@simonflarup.dk>
2019-10-05 02:37:35 +0200

    added map info

--- map_gen/maps/concrete_jungle.lua
+++ map_gen/maps/concrete_jungle.lua
@@ -1,8 +1,16 @@
 local floor = math.floor
-
 local RestrictEntities = require 'map_gen.shared.entity_placement_restriction'
 local Event = require 'utils.event'
 local b = require 'map_gen.shared.builders'
+
+local ScenarioInfo = require 'features.gui.info'
+
+ScenarioInfo.set_map_name('Concrete Jungle')
+ScenarioInfo.set_map_description([[
+Extensive underground mining have resulted in brittle soil.
+New regulations require heavy objects being placed on top,
+proper materials to support the soil!
+]])
 
 --- Items explicitly allowed on ores
 RestrictEntities.add_allowed(
@@ -82,6 +90,46 @@ local tier_3 = {
     ['logistic-chest-requester'] = true
 }
 
+local tier_2_items = '[item=rail]'
+
+for k, _ in pairs(tier_2) do
+    if not (k == 'straight-rail' or k == 'curved-rail') then
+        tier_2_items = tier_2_items .. ' [item=' .. k .. ']'
+    end
+end
+
+local tier_3_items = ''
+
+local tier_3_counter = 0
+for k, _ in pairs(tier_3) do
+    tier_3_items = tier_3_items .. ' [item=' .. k .. ']'
+    tier_3_counter = tier_3_counter + 1
+    if tier_3_counter > 14 then
+        tier_3_counter = 0
+        tier_3_items = tier_3_items .. '\n'
+    end
+end
+
+ScenarioInfo.add_map_extra_info(
+    [[
+You may only build the factory on [item=stone-brick] [item=concrete] [item=refined-concrete].
+
+Exceptions:
+ [item=burner-mining-drill] [item=pumpjack] [item=small-electric-pole] [item=car] [item=tank] [item=pipe] [item=pipe-to-ground] [item=offshore-pump]
+ [item=transport-belt] [item=fast-transport-belt] [item=express-transport-belt]  [item=underground-belt] [item=fast-underground-belt] [item=express-underground-belt]
+
+Some buildings/entities require better ground support!
+
+
+Ground support minimum concrete:
+]] ..
+        tier_2_items .. [[
+
+
+Ground support minimum refined concrete:
+]] .. tier_3_items
+)
+
 --- The logic for checking that there are resources under the entity's position
 RestrictEntities.set_keep_alive_callback(
     function(entity)
@@ -120,12 +168,12 @@ RestrictEntities.set_keep_alive_callback(
     end
 )
 
---- Warning for players when their entities are destroyed
+--- Warning for players when their entities are destroyed (needs to be pre because of the stack)
 local function on_destroy(event)
     local p = game.get_player(event.player_index)
     local name = event.stack.name
     if p and p.valid then
-        if not(name == 'blueprint') then
+        if not (name == 'blueprint') then
             local tier = 'stone path'
             if (tier_2[name]) then
                 tier = 'concrete'
