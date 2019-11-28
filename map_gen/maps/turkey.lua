97be613e3d26b5c1272fe0b5c627cf1bd66d8885
Zahir Khan <zahiryk@gmail.com>
2019-11-27 20:26:37 -0500

    Added random turkey message every 10min

--- map_gen/maps/turkey.lua
+++ map_gen/maps/turkey.lua
@@ -2,11 +2,23 @@ local b = require "map_gen.shared.builders"
 local pic = require "map_gen.data.presets.turkey"
 local RS = require 'map_gen.shared.redmew_surface'
 local MGSP = require 'resources.map_gen_settings'
+local Event = require 'utils.event'
+local turkey_message_random = require 'resources.turkey_messages'
 
 RS.set_map_gen_settings(
     {
         MGSP.cliff_none
     }
+)
+
+Event.add(
+    defines.events.on_tick,
+    function(event)
+	    if event.tick % 60*600 == 0 then
+            local message = turkey_message_random[math.random(#turkey_message_random)]
+            game.print(message)
+	    end
+    end
 )
 
 pic = b.decompress(pic)
