54fa0be54b751798013482344d2843fb772952ec
Bluerock <write.mikeadams@gmail.com>
2020-02-16 12:27:57 -0500

    Prevent oil from spawning too close to other oil patches.

--- map_gen/maps/diggy/feature/scattered_resources.lua
+++ map_gen/maps/diggy/feature/scattered_resources.lua
@@ -34,6 +34,18 @@ local function get_name_by_weight(collection, sum)
     Debug.print('Current \'' .. current .. '\' should be higher or equal to random \'' .. target .. '\'')
 end
 
+--overcrowded_resource returns true if one or more entity <resource_name> is found within <min_gap> tiles from <position>
+local function overcrowded_resource(surface, position, resource_name, min_gap)
+    local d = min_gap
+    local bounding_box = {{x = position.x - d + 1, y = position.y - d + 1}, {x = position.x + d, y = position.y + d}}
+    local found_resources = surface.find_entities_filtered{area = bounding_box, name = resource_name}
+    if #found_resources == 0 then
+        return false
+    else
+        return true
+    end
+end
+
 --[[--
     Registers all event handlers.
 ]]
@@ -134,6 +146,14 @@ function ScatteredResources.register(config)
             return false
         end
 
+        local position = {x = x, y = y}
+        if resource_name == 'crude-oil' then
+            local min_gap = 2  --must be at least this many tiles between crude-oil patches
+            if overcrowded_resource(surface, position, resource_name, min_gap) then
+                return false  --when overcrowded_resource is true, skip this resource spawn
+            end
+        end
+
         local range = resource_richness_values[get_name_by_weight(resource_richness_weights, resource_richness_weights_sum)]
         local amount = random(range[1], range[2]) * (1 + ((distance / cluster.distance_richness) * 0.01)) * cluster.yield
 
@@ -141,7 +161,7 @@ function ScatteredResources.register(config)
             amount = amount * resource_type_scalar[resource_name]
         end
 
-        template_resources(surface, {{name = resource_name, position = {x = x, y = y}, amount = ceil(amount)}})
+        template_resources(surface, {{name = resource_name, position = position, amount = ceil(amount)}})
         return true
     end
 
