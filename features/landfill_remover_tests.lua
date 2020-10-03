031517fb8587db5895fb02fe6a83d4f9b46d1b3a
James Gillham <jrdgillham@gmail.com>
2020-10-03 14:37:56 +0100

    More landfill remover tests

--- features/landfill_remover_tests.lua
+++ features/landfill_remover_tests.lua
@@ -45,6 +45,61 @@ Declare.module(
             return cursor
         end
 
+        local function setup_player_with_normal_selection_mode_deconstruction_planner(player)
+            local inventory = player.get_inventory(main_inventory)
+            inventory.clear()
+            inventory.insert('deconstruction-planner')
+            local stack = inventory.find_item_stack('deconstruction-planner')
+            stack.set_tile_filter(1, 'landfill')
+            stack.tile_selection_mode = defines.deconstruction_item.tile_selection_mode.normal
+
+            local cursor = player.cursor_stack
+            cursor.set_stack(stack)
+
+            return cursor
+        end
+
+        local function setup_player_with_never_selection_mode_deconstruction_planner(player)
+            local inventory = player.get_inventory(main_inventory)
+            inventory.clear()
+            inventory.insert('deconstruction-planner')
+            local stack = inventory.find_item_stack('deconstruction-planner')
+            stack.set_tile_filter(1, 'landfill')
+            stack.tile_selection_mode = defines.deconstruction_item.tile_selection_mode.never
+
+            local cursor = player.cursor_stack
+            cursor.set_stack(stack)
+
+            return cursor
+        end
+
+        local function setup_player_with_always_selection_mode_deconstruction_planner(player)
+            local inventory = player.get_inventory(main_inventory)
+            inventory.clear()
+            inventory.insert('deconstruction-planner')
+            local stack = inventory.find_item_stack('deconstruction-planner')
+            stack.set_tile_filter(1, 'landfill')
+            stack.tile_selection_mode = defines.deconstruction_item.tile_selection_mode.always
+
+            local cursor = player.cursor_stack
+            cursor.set_stack(stack)
+
+            return cursor
+        end
+
+        local function setup_player_with_no_landfill_deconstruction_planner(player)
+            local inventory = player.get_inventory(main_inventory)
+            inventory.clear()
+            inventory.insert('deconstruction-planner')
+            local stack = inventory.find_item_stack('deconstruction-planner')
+            stack.tile_selection_mode = defines.deconstruction_item.tile_selection_mode.only
+
+            local cursor = player.cursor_stack
+            cursor.set_stack(stack)
+
+            return cursor
+        end
+
         Declare.test(
             'can remove landfill',
             function(context)
@@ -162,5 +217,218 @@ Declare.module(
                 end
             )
         end
+
+        Declare.test(
+            'does not remove landfill when out of reach',
+            function(context)
+                -- Arrange
+                local player = context.player
+                local surface = player.surface
+                local build_distance = player.build_distance + 5
+                local position = {build_distance, build_distance}
+                local area = {
+                    {build_distance + 0.1, build_distance + 0.1},
+                    {build_distance + 0.9, build_distance + 0.9}
+                }
+                surface.set_tiles({{name = 'landfill', position = position}})
+                local cursor = setup_player_with_valid_deconstruction_planner(player)
+
+                -- Act
+                EventFactory.do_player_deconstruct_area(cursor, player, area)
+
+                -- Assert
+                local tile = surface.get_tile(position[1], position[2])
+                Assert.equal('landfill', tile.name)
+            end
+        )
+
+        for _, item_name in pairs(tile_items) do
+            Declare.test(
+                'does not remove landfill when out of reach and covered by ' .. item_name,
+                function(context)
+                    -- Arrange
+                    local player = context.player
+                    local surface = player.surface
+                    local build_distance = player.build_distance + 5
+                    local position = {build_distance, build_distance}
+                    local area = {
+                        {build_distance + 0.1, build_distance + 0.1},
+                        {build_distance + 0.9, build_distance + 0.9}
+                    }
+                    surface.set_tiles({{name = 'landfill', position = position}})
+
+                    -- Place covering tile.
+                    local cursor = player.cursor_stack
+                    cursor.set_stack(item_name)
+                    player.build_from_cursor({position = position, terrain_building_size = 1})
+
+                    local before_tile = surface.get_tile(position[1], position[2])
+
+                    cursor = setup_player_with_valid_deconstruction_planner(player)
+
+                    -- Act
+                    EventFactory.do_player_deconstruct_area(cursor, player, area)
+
+                    -- Assert
+                    local tile = surface.get_tile(position[1], position[2])
+                    Assert.equal(before_tile.name, tile.name)
+                end
+            )
+        end
+
+        local tile_mode_test_cases = {
+            {
+                name = 'only',
+                setup = setup_player_with_valid_deconstruction_planner,
+                should_remove = true
+            },
+            {
+                name = 'normal',
+                setup = setup_player_with_normal_selection_mode_deconstruction_planner,
+                should_remove = true
+            },
+            {
+                name = 'always',
+                setup = setup_player_with_always_selection_mode_deconstruction_planner,
+                should_remove = true
+            },
+            {
+                name = 'never',
+                setup = setup_player_with_never_selection_mode_deconstruction_planner,
+                should_remove = false
+            },
+            {
+                name = 'no landfill',
+                setup = setup_player_with_no_landfill_deconstruction_planner,
+                should_remove = false
+            }
+        }
+
+        for _, test_case in pairs(tile_mode_test_cases) do
+            Declare.test(
+                'tile mode ' ..
+                    test_case.name .. ' should ' .. (test_case.should_remove and '' or 'not ') .. 'remove landfill',
+                function(context)
+                    -- Arrange
+                    local player = context.player
+                    local surface = player.surface
+                    local cursor = test_case.setup(player)
+                    local position = {2, 2}
+                    local area = {{2.1, 2.1}, {2.9, 2.9}}
+                    surface.set_tiles({{name = 'landfill', position = position}})
+                    local expected_tile = test_case.should_remove and config.revert_tile or 'landfill'
+
+                    -- Act
+                    EventFactory.do_player_deconstruct_area(cursor, player, area)
+
+                    -- Assert
+                    local tile = surface.get_tile(position[1], position[2])
+                    Assert.equal(expected_tile, tile.name)
+                end
+            )
+        end
+
+        local tile_mode_with_entity_test_cases = {
+            {
+                name = 'only',
+                setup = setup_player_with_valid_deconstruction_planner,
+                should_remove = true
+            },
+            {
+                name = 'normal',
+                setup = setup_player_with_normal_selection_mode_deconstruction_planner,
+                should_remove = false
+            },
+            {
+                name = 'always',
+                setup = setup_player_with_always_selection_mode_deconstruction_planner,
+                should_remove = true
+            },
+            {
+                name = 'never',
+                setup = setup_player_with_never_selection_mode_deconstruction_planner,
+                should_remove = false
+            },
+            {
+                name = 'no landfill',
+                setup = setup_player_with_no_landfill_deconstruction_planner,
+                should_remove = false
+            }
+        }
+
+        for _, test_case in pairs(tile_mode_with_entity_test_cases) do
+            Declare.test(
+                'tile mode ' ..
+                    test_case.name ..
+                        ' with entity should ' .. (test_case.should_remove and '' or 'not ') .. 'remove landfill',
+                function(context)
+                    -- Arrange
+                    local player = context.player
+                    local surface = player.surface
+
+                    local position1 = {2, 2}
+                    local position2 = {3, 2}
+                    local area = {{2.1, 2.1}, {3.9, 2.9}}
+                    surface.set_tiles(
+                        {{name = 'landfill', position = position1}, {name = 'landfill', position = position2}}
+                    )
+                    local expected_tile = test_case.should_remove and config.revert_tile or 'landfill'
+
+                    -- Place entity.
+                    local cursor = player.cursor_stack
+                    cursor.set_stack('iron-chest')
+                    player.build_from_cursor({position = position1})
+
+                    cursor = test_case.setup(player)
+
+                    -- Act
+                    EventFactory.do_player_deconstruct_area(cursor, player, area)
+
+                    -- Assert
+                    local tile = surface.get_tile(position2[1], position2[2])
+                    Assert.equal(expected_tile, tile.name)
+
+                    local entities = surface.find_entities(area)
+                    local entity = entities[1]
+                    Assert.is_lua_object_with_name(entity, 'iron-chest', 'iron-chest was not valid.')
+                    entity.destroy()
+                end
+            )
+        end
+
+        Declare.test(
+            'ignore character when removing landfill',
+            function(context)
+                -- Arrange
+                local player = context.player
+                local surface = player.surface
+                local cursor = setup_player_with_valid_deconstruction_planner(player)
+                local positions = {
+                    {-1, -1},
+                    {-1, 0},
+                    {0, -1},
+                    {0, 0}
+                }
+                local area = {{-1.5, -1.5}, {0.5, 0.5}}
+
+                surface.set_tiles(
+                    {
+                        {name = 'landfill', position = positions[1]},
+                        {name = 'landfill', position = positions[2]},
+                        {name = 'landfill', position = positions[3]},
+                        {name = 'landfill', position = positions[4]}
+                    }
+                )
+
+                -- Act
+                EventFactory.do_player_deconstruct_area(cursor, player, area)
+
+                -- Assert
+                for _, pos in pairs(positions) do
+                    local tile = surface.get_tile(pos[1], pos[2])
+                    Assert.equal(config.revert_tile, tile.name)
+                end
+            end
+        )
     end
 )
