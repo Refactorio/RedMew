31601c4ec33a3ed15c4234de28f9eca199d51911
James Gillham <jrdgillham@gmail.com>
2020-10-04 11:44:50 +0100

    include ignores deflated files

--- utils/test/include.lua
+++ utils/test/include.lua
@@ -1,9 +1,14 @@
 local require = require
 local pcall = pcall
+local find = string.find
+
+local function file_is_missing(message)
+    return find(message, 'no such file') or find(message, 'File was removed to decrease save file size')
+end
 
 return function(name)
     local s, e = pcall(require, name)
-    if not s and not string.find(e, 'no such file') then
+    if not s and not file_is_missing(e) then
         error(e, 2)
     end
 end
