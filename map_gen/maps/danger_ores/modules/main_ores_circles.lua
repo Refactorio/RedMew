ca77b4ce3b535c87935e24898ce53655a4654989
Soggs <soggsthemage@gmail.com>
2021-05-19 22:53:13 +0200

    Clean up and improvment to danger_ores_circles.

--- map_gen/maps/danger_ores/modules/main_ores_circles.lua
+++ map_gen/maps/danger_ores/modules/main_ores_circles.lua
@@ -6,23 +6,28 @@ local binary_search = table.binary_search
 local bnot = bit32.bnot
 local sqrt = math.sqrt
 local floor = math.floor
-
+local value = b.manhattan_value
 
 return function(config)
     local main_ores = config.main_ores
     local main_ores_split_count = config.main_ores_split_count or 1
+    local start_ore_shape = config.start_ore_shape
 
     main_ores = Helper.split_ore(main_ores, main_ores_split_count)
+    
+    if config.main_ores_shuffle_order then
+      table.shuffle_table(main_ores)
+    end
 
     local function condition_factory(ore_name)
+      local scale = config.circle_scale or 1
+      --local randomize_scale = config.randomize_scale or false
+      local ore_table = {}
+      for _, ore_data in pairs(main_ores) do
+        table.insert(ore_table, {name = ore_data.name, weight = ore_data.weight})
+      end
+      local weighted = b.prepare_weighted_array(ore_table)
       return function(x, y, _)
-        local scale = config.circle_scale or 1
-        --local randomize_scale = config.randomize_scale or false
-        local ore_table = {}
-        for _, ore_data in pairs(main_ores) do
-          table.insert(ore_table, {name = ore_data.name, weight = ore_data.weight})
-        end
-        local weighted = b.prepare_weighted_array(ore_table)
         local i =  floor(sqrt(x * x + y * y) * scale) % weighted.total + 1
         local index = binary_search(weighted, i)
         if index < 0 then
@@ -35,10 +40,13 @@ return function(config)
     return function(tile_builder, ore_builder, spawn_shape, water_shape, _)
 
       local shapes = {}
+      local starting_ores_list = {}
       for _, ore_data in pairs(main_ores) do
           local ore_name = ore_data.name
           local tiles = ore_data.tiles
           local land = tile_builder(tiles)
+          
+          table.insert(starting_ores_list, b.resource(start_ore_shape, ore_name, value(100, 0)))
 
           local ratios = ore_data.ratios
           local weighted = b.prepare_weighted_array(ratios)
@@ -52,6 +60,8 @@ return function(config)
 
       local ores = b.any(shapes)
 
-      return b.any {spawn_shape, water_shape, ores}
+      local starting_ores = b.segment_pattern(starting_ores_list)  
+
+      return b.any {spawn_shape, starting_ores ,water_shape, ores}
     end
 end
