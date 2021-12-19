2a560eada2fee7b3520a4478863ae8b9a5ee5e88
Jayefuu <28716932+Jayefuu@users.noreply.github.com>
2021-12-19 05:49:46 +0000

    Updates as per request
    - added localisation for restart message
    - fixed discord tag formatting
    - added some colour feedback for admins to make it easier to see the message among the restart messages.

--- map_gen/maps/diggy/feature/restart_command.lua
+++ map_gen/maps/diggy/feature/restart_command.lua
@@ -3,6 +3,7 @@ local Server = require 'features.server'
 local Core = require 'utils.core'
 local Restart = require 'features.restart_command'
 local Global = require 'utils.global'
+local Color = require 'resources.color_presets'
 
 local restart_difficulty
 
@@ -17,11 +18,11 @@ end)
 return function(config)
     -- Use these on live to ping @diggy role in #map-promotion channel
     local map_promotion_channel = Discord.channel_names.map_promotion
-    local danger_ore_role_mention = Discord.role_mentions.diggy
+    local diggy_role_mention = Discord.role_mentions.diggy
 
     -- Use these instead if testing new restart features so as to not ping @diggy role:
     --local map_promotion_channel = Discord.channel_names.bot_playground
-    --local danger_ore_role_mention = Discord.role_mentions.test
+    --local diggy_role_mention = Discord.role_mentions.test
 
     Restart.set_start_game_data({type = Restart.game_types.scenario, name = config.scenario_name or 'diggy-next'})
 
@@ -29,14 +30,14 @@ return function(config)
         local adjusted_difficulty = restart_difficulty - 1 --because the labels in game are different from the technologies[].level. This adjusts the printed messages so it's correct for player.
         if player.admin then
             if game.player.force.technologies["mining-productivity-4"].level < restart_difficulty  then
-                player.print("Victory condition overriden. Did not reach Mining Productivity "..adjusted_difficulty)
+                player.print("Victory condition overriden. Did not reach Mining Productivity "..adjusted_difficulty,Color.fail)
             else
-                player.print("Victory condition reached: Mining Productivity: "..adjusted_difficulty)
+                player.print("Victory condition reached: Mining Productivity: "..adjusted_difficulty,Color.success)
             end
             return true
         end
         if game.player.force.technologies["mining-productivity-4"].level < restart_difficulty  then
-            player.print("Mine harder dwarf! You must reach Mining Productivity "..adjusted_difficulty.." to start a new mining venture!")
+            game.print({'command_description.diggy_restart_condition_not_met', adjusted_difficulty},Color.fail)
             return false
         end
         return true
@@ -49,7 +50,7 @@ return function(config)
         local time_string = Core.format_time(game.ticks_played)
 
         local message = {
-            danger_ore_role_mention,
+            diggy_role_mention,
             ' **Diggy has just restarted! Previous map lasted: ',
             time_string,
             '!\\n',
