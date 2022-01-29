e0b580a60487d86c06b3f42040b3be35b9c7bc7c
James Gillham <jrdgillham@gmail.com>
2022-01-29 16:10:45 +0000

    Add danger ore map poll.

--- map_gen/maps/danger_ores/presets/danger_ore_one_direction_beltboxes_ore_only.lua
+++ map_gen/maps/danger_ores/presets/danger_ore_one_direction_beltboxes_ore_only.lua
@@ -135,6 +135,8 @@ concrete_on_landfill({tile = 'blue-refined-concrete'})
 local remove_non_ore_stacked_recipes = require 'map_gen.maps.danger_ores.modules.remove_non_ore_stacked_recipes'
 remove_non_ore_stacked_recipes()
 
+require 'map_gen.maps.danger_ores.modules.map_poll'
+
 local main_ores_builder = require 'map_gen.maps.danger_ores.modules.main_ores_one_direction'
 
 local function post_map_func(map_shape)
