39a0d0fb540da492152be92aefbdc8b200f73bfb
grilledham <jrdgillham@gmail.com>
2022-03-12 18:12:01 +0000

    Add missing map info for crashsite maps. (#1298)

--- map_gen/maps/crash_site/presets/manhattan.lua
+++ map_gen/maps/crash_site/presets/manhattan.lua
@@ -1,4 +1,5 @@
 local b = require 'map_gen.shared.builders'
+local ScenarioInfo = require 'features.gui.info'
 local MGSP = require 'resources.map_gen_settings'
 
 local type = type
@@ -36,6 +37,18 @@ local config = {
 }
 
 local Scenario = require 'map_gen.maps.crash_site.scenario'
+ScenarioInfo.set_map_name('Crashsite Manhattan')
+ScenarioInfo.set_map_description('Capture outposts and defend against the biters.')
+ScenarioInfo.add_map_extra_info([[
+    - A Manhattan map version of Crash Site.
+    - Outposts have enemy turrets defending them.
+    - Outposts have loot and provide a steady stream of resources.
+    - Outpost markets to purchase items and outpost upgrades.
+    - Capturing outposts increases evolution.
+    - Reduced damage by all player weapons, turrets, and ammo.
+    - Biters have more health and deal more damage.
+    - Biters and spitters spawn on death of entities.
+    ]])
 local crashsite = Scenario.init(config)
 
 local function get_tile_name(tile)
