c7644814e18fad6bfae7b67905644a50d40b0732
FuzTheCat <96272894+FuzTheCat@users.noreply.github.com>
2022-06-17 17:24:24 +1000

    Fix warnings in belts_n_bullets.lua

--- map_gen/maps/diggy/feature/belts_n_bullets.lua
+++ map_gen/maps/diggy/feature/belts_n_bullets.lua
@@ -1,10 +1,8 @@
 -- This module disables some technologies to focus on belts and bullets
 local Event = require 'utils.event'
-local Token = require 'utils.token'
-local ScenarioInfo = require 'features.gui.info'
+-- local ScenarioInfo = require 'features.gui.info'
 
 local BeltsnBullets = {}
-
 
 function BeltsnBullets.register (config)
 	local disabled_technologies = config.disabled_technologies
@@ -13,7 +11,6 @@ function BeltsnBullets.register (config)
 		    game.forces.player.technologies[dis_tech].enabled = false
 		end
 	end)
-	
 end
 
 return BeltsnBullets
