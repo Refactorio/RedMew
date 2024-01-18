db8c21185209425635e15666b6d63191c7af576e
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-01-18 19:56:50 +0100

    Adjust popup max length & offset

--- features/popup_chat.lua
+++ features/popup_chat.lua
@@ -5,6 +5,7 @@ local config = global.config.popup_chat
 local MIN_LIFETIME = config.min_lifetime or 06 * 60 -- 06s
 local MAX_LIFETIME = config.max_lifetime or 20 * 60 -- 20s
 local MIN_MESSAGE_LENGTH = 40
+local MAX_MESSAGE_LENGTH = 92
 local TIME_PER_CHAR = 3 -- about +1 sec every 20 chars (60/20 ticks/chars)
 
 local data = {
@@ -23,6 +24,15 @@ local function message_lifetime(message)
   end
   local extra_time = math.floor((length - MIN_MESSAGE_LENGTH) * TIME_PER_CHAR)
   return math.min(MIN_LIFETIME + extra_time, MAX_LIFETIME)
+end
+
+---@param message string
+local function safe_message(message)
+  local length = message:len()
+  if length <= MAX_MESSAGE_LENGTH then
+    return message
+  end
+  return string.sub(message, 1, MAX_MESSAGE_LENGTH) .. '[...]'
 end
 
 ---@param event defines.event.on_console_chat
@@ -48,10 +58,10 @@ local function on_console_chat(event)
   color.a = 0.9
 
   popup_ID = rendering.draw_text({
-    text = message,
+    text = safe_message(message),
     surface = player.surface,
     target = player.character,
-    target_offset = {0, -3},
+    target_offset = {0, -4},
     color = color,
     font = 'compilatron-message-font',
     scale = 1.75,
