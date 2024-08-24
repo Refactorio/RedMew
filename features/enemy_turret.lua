c5b1c2845852954b44f31ec97bc2ad105887eac3
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-08-24 13:16:04 +0200

    Artillery targeting

--- features/enemy_turret.lua
+++ features/enemy_turret.lua
@@ -1,23 +1,74 @@
 local Event = require 'utils.event'
 local Global = require 'utils.global'
+local Task = require 'utils.task'
+local Token = require 'utils.token'
 local register_on_entity_destroyed = script.register_on_entity_destroyed
 
 local Public = {}
 local turrets_map = {}
 local register_map = {}
 local primitives = { index = nil }
+local Artillery = {
+  enabled = true,
+  turret_name = 'artillery-turret',
+  character_projectile = 'artillery-projectile',
+  structure_projectile = 'rocket',
+  target_force = 'player',
+  last_fire = 0,
+  fire_interval = 60, -- ticks
+  cooldown = 480, -- ticks
+  min_range = 32, -- tiles
+  max_range = 224, -- tiles
+  max_targeting_attempts = 10,
+  range_modifier = nil,
+  manual_range_modifier = nil,
+  creation_distance = 1.6,
+  target_offset = { -0.15625, -0.07812 },
+  target_entities = {
+    'artillery-turret',
+    'artillery-wagon',
+    'car',
+    'cargo-wagon',
+    'character',
+    'flamethrower-turret',
+    'fluid-wagon',
+    'furnace',
+    'gun-turret',
+    'lab',
+    'laser-turret',
+    'locomotive',
+    'radar',
+    'silo',
+    'spidertron',
+    'tank',
+  },
+}
 
 Global.register(
   {
     turrets_map = turrets_map,
     register_map = register_map,
     primitives = primitives,
+    artillery = Artillery,
   },
   function(tbl)
     turrets_map = tbl.turrets_map
     register_map = tbl.register_map
     primitives = tbl.primitives
+    Artillery = tbl.artillery
 end)
+
+local function distance(posA, posB)
+  return math.sqrt((posA.x - posB.x)^2 + (posA.y - posB.y)^2)
+end
+
+function Public.get_artillery_settings()
+  return Artillery
+end
+
+function Public.set_artillery_settings(key, value)
+  Artillery[key] = value
+end
 
 function Public.register(entity, refill_name)
   if not (entity and entity.valid) then
@@ -30,8 +81,9 @@ function Public.register(entity, refill_name)
 
   local is_item = game.item_prototypes[refill_name] and true or false
   local is_fluid = game.fluid_prototypes[refill_name] and true or false
+  local is_artillery = entity.prototype.type == Artillery.turret_name
 
-  if not (is_item or is_fluid )then
+  if not (is_item or is_fluid or is_artillery) then
     return
   end
 
@@ -42,9 +94,13 @@ function Public.register(entity, refill_name)
     entity = entity,
     refill = refill_name,
     is_fluid = is_fluid,
+    is_artillery = is_artillery,
     destroy_id = destroy_id
   }
 
+  if data.is_artillery then
+    data.next_fire = game.tick
+  end
   if data.is_fluid then
     data.fluid_stack = {
       name = data.refill,
@@ -78,6 +134,65 @@ function Public.reset()
   end
 end
 
+local artillery_projectile_token = Token.register(function(data)
+  local surface = game.get_surface(data.surface_index)
+  local target = data.target
+  local source = data.source
+  if not (surface and surface.valid) then return end
+  if not (target and target.valid) then return end
+  if not (source and source.valid) then return end
+  surface.create_entity{
+    name = data.name,
+    position = data.position,
+    target = target,
+    source = source,
+    force = 'enemy',
+    speed = 1.5,
+  }
+end)
+
+local function simulate_automatic_artillery(data)
+  if data.next_fire > game.tick then
+    return
+  end
+
+  local entity = data.entity
+  local surface = entity.surface
+  local source_position = entity.position
+  local range_modifier = Artillery.range_modifier or entity.force.artillery_range_modifier or 0
+  local manual_range_modifier = Artillery.manual_range_modifier or entity.prototype.manual_range_modifier or 0
+  local params = {
+    position = source_position,
+    radius = Artillery.max_range * (1 + range_modifier) * (1 + manual_range_modifier),
+    name = Artillery.target_entities,
+    force = Artillery.target_force,
+    limit = Artillery.max_targeting_attempts,
+  }
+  local targets = surface.find_entities_filtered(params)
+
+  if #targets == 0 then
+    return
+  end
+
+  for i = 1, #targets do
+    local target = targets[i]
+    if distance(source_position, target.position) > Artillery.min_range then
+      Task.set_timeout_in_ticks(Artillery.fire_interval, artillery_projectile_token, {
+        surface_index = surface.index,
+        name = target.name == 'character' and Artillery.character_projectile or Artillery.structure_projectile,
+        position = target.position,
+        target = target,
+        source = entity,
+        force = 'enemy',
+        speed = 1.5,
+      })
+      data.next_fire = game.tick + Artillery.cooldown
+      Artillery.last_fire = game.tick
+      break
+    end
+  end
+end
+
 local function on_entity_destroyed(event)
   local destroy_id = event.registration_number
   local unit_id = event.unit_number
@@ -99,6 +214,9 @@ local function on_tick()
     return
   end
 
+  if Artillery.enabled and data.is_artillery and ((Artillery.last_fire + Artillery.fire_interval) < game.tick) then
+    simulate_automatic_artillery(data)
+  end
   if data.is_fluid then
     data.entity.insert_fluid(data.fluid_stack)
   else
