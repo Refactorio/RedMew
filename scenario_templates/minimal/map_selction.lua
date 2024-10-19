7713b62a8eb42d776e1a6f96d5adc21be8892ac7
James Gillham <jrdgillham@gmail.com>
2024-10-19 13:42:30 +0100

    Add minimal scenario

---
+++ scenario_templates/minimal/map_selction.lua
@@ -0,0 +1,7 @@
+local config = require 'config'
+config.redmew_surface.enabled = false
+config.market.enabled = false
+config.reactor_meltdown.enabled = false
+config.player_create.starting_items = {}
+config.player_rewards.enabled = false
+config.apocalypse.enabled = false
