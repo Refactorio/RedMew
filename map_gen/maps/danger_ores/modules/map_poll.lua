8fb4e53036c66a2a4accae22936a190dec72fcf7
RedRafe <gabri97.gt+redrafe@gmail.com>
2024-12-30 11:59:23 +0100

    Fix omnimatter name

--- map_gen/maps/danger_ores/modules/map_poll.lua
+++ map_gen/maps/danger_ores/modules/map_poll.lua
@@ -49,7 +49,7 @@ local maps = {
     { name = 'danger-ore-lazy-one', display_name = 'Lazy One (no handcraft)', mod_pack = mod_packs.danger_ore_normal },
     { name = 'danger-ore-normal-science', display_name = 'Normal science (+ biters)', mod_pack = mod_packs.danger_ore_normal },
     { name = 'danger-ore-omnimatter', display_name = 'Omnimatter (1 ore)', mod_pack = mod_packs.danger_ore_omnimatter },
-    { name = 'danger-ore-danger_ore_omnimattermatter-cages', display_name = 'Omnimatter Cages (1 ore + frames)', mod_pack = mod_packs.danger_ore_omnimatter },
+    { name = 'danger-ore-omnimatter-cages', display_name = 'Omnimatter Cages (1 ore + frames)', mod_pack = mod_packs.danger_ore_omnimatter },
     { name = 'danger-ore-one-direction', display_name = 'One Direction (right ribbon world)', mod_pack = mod_packs.danger_ore_normal },
     { name = 'danger-ore-one-direction-wide', display_name = 'One Direction Wide (wide right ribbon world)', mod_pack = mod_packs.danger_ore_normal },
     { name = 'danger-ore-patches', display_name = 'Patches (ore islands in coal)', mod_pack = mod_packs.danger_ore_normal },
