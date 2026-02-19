df3dcb88981abc272d6dbca15173896d48bb1e08
RedRafe <gabri97.gt+redrafe@gmail.com>
2026-02-19 23:58:24 +0100

    Address safety edge cases

--- map_gen/maps/diggy/feature/blueprint_tools.lua
+++ map_gen/maps/diggy/feature/blueprint_tools.lua
@@ -16,6 +16,7 @@
     - turn On/Off individual entities
 ]]
 
+local Command = require 'utils.command'
 local Event = require 'utils.event'
 local Global = require 'utils.global'
 local Gui = require 'utils.gui'
@@ -35,6 +36,11 @@ local feature_switch_name = Gui.uid_name()
 local entity_button_name = Gui.uid_name()
 local tiles_checkbox_name = Gui.uid_name()
 local show_window_checkbox_name = Gui.uid_name()
+
+local admin_main_frame_name = Gui.uid_name()
+local admin_feature_switch_name = Gui.uid_name()
+local admin_entity_button_name = Gui.uid_name()
+local admin_close_button_name = Gui.uid_name()
 
 local Public = {
     setting_name = 'diggy_blueprint_tools',
@@ -128,7 +134,7 @@ local function on_player_cursor_stack_changed(event)
 
     -- Regulars+ feature only
     if Rank.less_than(player.name, required_rank) then
-        return
+        return Public.toggle_main_frame(player, false)
     end
 
     -- Player has hidden the window
@@ -171,14 +177,11 @@ local function on_player_cursor_stack_changed(event)
     if ENTITIES[cursor_stack.name] ~= nil then
         return Public.toggle_main_frame(player, true)
     end
+
+    Public.toggle_main_frame(player, false)
 end
 
 local function on_marked_for_deconstruction(event)
-    local ps = get_player_settings(event.player_index)
-    if not ps.enabled then
-        return
-    end
-
     local entity = event.entity
     local name = entity.name
 
@@ -186,17 +189,8 @@ local function on_marked_for_deconstruction(event)
         name = entity.surface.get_tile(entity.position.x, entity.position.y).name
     end
 
-    if is_support_beam(name) and ps.entities[name] then
-        entity.cancel_deconstruction(game.get_player(event.player_index).force)
-    end
-end
-
-local function on_setting_set(event)
-    if event.setting_name ~= Public.setting_name then
-        return
-    end
-
-    if not event.value_changed then
+    -- Entity is not a support
+    if not is_support_beam(name) then
         return
     end
 
@@ -205,10 +199,142 @@ local function on_setting_set(event)
         return
     end
 
-    Public.toggle_main_frame(player, event.new_value)
+    -- Regular+ feature only
+    if Rank.less_than(player.name, required_rank) then
+        return entity.cancel_deconstruction(player.force)
+    end
+
+    local ps = get_player_settings(player.index)
+    if not ps.enabled then
+        return
+    end
+
+    -- Entity is not marked to keep safe
+    if not ps.entities[name] then
+        return
+    end
+
+    entity.cancel_deconstruction(player.force)
+end
+
+local function on_setting_set(event)
+    if event.setting_name ~= Public.setting_name then
+        return
+    end
+
+    local player = game.get_player(event.player_index)
+    if not player then
+        return
+    end
+
+    local should_show_gui = Rank.equal_or_greater_than(player.name, required_rank) and event.new_value and event.value_changed
+    Public.toggle_main_frame(player, should_show_gui)
 
     local frame = Gui.get_left_element(player, main_frame_name)
-    Gui.get_data(frame).show_window_checkbox.state = event.new_value
+    Gui.get_data(frame).show_window_checkbox.state = should_show_gui
+end
+
+local function get_admin_main_frame(_, player)
+    local frame = player.gui.screen[admin_main_frame_name]
+    if frame and frame.valid then
+        Gui.destroy(frame)
+    end
+
+    frame = player.gui.screen.add {
+        type = 'frame',
+        name = admin_main_frame_name,
+        direction = 'vertical',
+    }
+    Gui.set_style(frame, { maximal_height = 930 })
+
+    do -- Header
+        local header = frame.add { type = 'flow', direction = 'horizontal' }
+        Gui.set_style(header, { horizontal_spacing = 8, vertical_align = 'center', bottom_padding = 4 })
+
+        local label = header.add { type = 'label', caption = 'Blueprint tools management', style = 'frame_title' }
+        label.drag_target = frame
+
+        local dragger = header.add { type = 'empty-widget', style = 'draggable_space_header' }
+        dragger.drag_target = frame
+        Gui.set_style(dragger, { height = 24, horizontally_stretchable = true })
+
+        local button = header.add {
+            type = 'sprite-button',
+            name = admin_close_button_name,
+            sprite = 'utility/close',
+            clicked_sprite = 'utility/close_black',
+            style = 'close_button',
+            tooltip = {'gui.close-instruction'}
+        }
+        Gui.set_data(button, frame)
+    end
+
+    do -- Content
+        local inner = frame.add { type = 'frame', style = 'inside_deep_frame', direction = 'vertical' }
+        local online = inner
+            .add { type = 'frame', style = 'subheader_frame'}
+            .add { type = 'label', caption = ('Online: %d/%d'):format(#game.connected_players, #game.players), style = 'subheader_label' }
+        Gui.set_style(online.parent, { horizontally_stretchable = true })
+
+        local grid = inner
+            .add { type = 'scroll-pane' }
+            .add { type = 'table', style = 'table_with_selection', column_count = 4 }
+
+        local function make_rank(name)
+            local label = grid.add { type = 'label', caption = Rank.get_player_rank_name(name) }
+            Gui.set_style(label, { font_color = Rank.get_player_rank_color(name) })
+        end
+
+        local function make_switch(index)
+            local ps = get_player_settings(index)
+            grid.add { type = 'flow' }.add {
+                type = 'switch',
+                name = admin_feature_switch_name,
+                switch_state = switch_state_map[ps.enabled],
+                left_label_caption = 'On',
+                right_label_caption = 'Off',
+                left_label_tooltip = 'Use safe deconstruction mode',
+                right_label_tooltip = 'Ignore safe deconstruction mode',
+                tags = { index = index },
+            }
+        end
+
+        local function make_list(index)
+            local ps = get_player_settings(index)
+
+            local list = grid
+                .add { type = 'scroll-pane', style = 'naked_scroll_pane' }
+                .add { type = 'table', column_count = 10, style = 'filter_slot_table' }
+
+            for name, status in pairs(ps.entities) do
+                list.add{ type = 'flow' }.add{
+                    type = 'sprite-button',
+                    name = admin_entity_button_name,
+                    style = 'slot_button',
+                    auto_toggle = true,
+                    toggled = status,
+                    sprite = get_sprite_from_name(name),
+                    tags = { name = name, index = index },
+                    tooltip = get_tooltip_from_name(name),
+                }
+            end
+        end
+
+        grid.add { type = 'label', caption = 'Name' }
+        grid.add { type = 'label', caption = 'Rank' }
+        grid.add { type = 'label', caption = 'Status' }
+        grid.add { type = 'label', caption = 'Entities' }
+
+        for index, p in pairs(game.players) do
+            grid.add { type = 'label', caption = p.name }
+            make_rank(p.name)
+            make_switch(index)
+            make_list(index)
+        end
+    end
+
+    frame.force_auto_center()
+    player.opened = frame
 end
 
 ---@param player LuaPlayer
@@ -227,10 +353,15 @@ Public.get_main_frame = function(player)
         name = main_frame_name,
         type = 'frame',
         direction = 'vertical',
-        caption = 'Blueprint tools',
-        tooltip = 'You can enable/disable this window from RedMew Settings > Show blueprint tools'
     })
     Gui.set_style(frame, { maximal_width = 228 })
+
+    frame.add {
+        type = 'label',
+        style = 'frame_title',
+        caption = 'Blueprint tools [img=info]',
+        tooltip = 'You can enable/disable this window from \nRedMew Settings > Show blueprint tools'
+    }
 
     local inner = frame
         .add { type = 'frame', style = 'inside_shallow_frame', direction = 'vertical' }
@@ -241,7 +372,7 @@ Public.get_main_frame = function(player)
         type = 'switch',
         name = feature_switch_name,
         caption = 'Feature caption',
-        state = switch_state_map[ps.enabled],
+        switch_state = switch_state_map[ps.enabled],
         left_label_caption = 'On',
         right_label_caption = 'Off',
         left_label_tooltip = 'Use safe deconstruction mode',
@@ -261,7 +392,7 @@ Public.get_main_frame = function(player)
         type = 'checkbox',
         name = show_window_checkbox_name,
         caption = { 'diggy.blueprint_tools_tooltip' },
-        state = should_show_gui
+        state = Settings.get(player.index, Public.setting_name)
     }
     Gui.set_data(data.show_window_checkbox, data)
 
@@ -286,14 +417,12 @@ Public.get_main_frame = function(player)
             sprite = get_sprite_from_name(name),
             tags = { name = name },
             tooltip = get_tooltip_from_name(name),
+            enabled = ps.enabled,
         }
     end
 
     Gui.set_data(frame, data)
     frame.visible = should_show_gui
-
-    -- must be set after the frame/inner-frame have been created
-    Settings.set(player.index, Public.setting_name, should_show_gui)
 
     return frame
 end
@@ -357,6 +486,53 @@ Public.register = function()
 
     Gui.on_click(entity_button_name, function(event)
         get_player_settings(event.player_index).entities[event.element.tags.name] = event.element.toggled
+    end)
+
+    Command.add(
+        'blueprint-tools',
+        {
+            description = { 'command_description.blueprint_tools' },
+            required_rank = Ranks.moderator,
+            allowed_by_server = false,
+        },
+        get_admin_main_frame
+    )
+
+    Gui.on_click(admin_close_button_name, function(event)
+        Gui.destroy(Gui.get_data(event.element))
+    end)
+
+    Gui.on_custom_close(admin_main_frame_name, function(event)
+        Gui.destroy(event.element)
+    end)
+
+    Gui.on_switch_state_changed(admin_feature_switch_name, function(event)
+        local state = switch_state_map[event.element.switch_state]
+        local player_index = event.element.tags.index
+        get_player_settings(player_index).enabled = state
+
+        local target = game.get_player(player_index)
+        if not target then
+            return
+        end
+        local frame = Gui.get_left_element(target, main_frame_name)
+        if frame then
+            Gui.destroy(frame)
+        end
+    end)
+
+    Gui.on_click(admin_entity_button_name, function(event)
+        local player_index = event.element.tags.index
+        get_player_settings(player_index).entities[event.element.tags.name] = event.element.toggled
+
+        local target = game.get_player(player_index)
+        if not target then
+            return
+        end
+        local frame = Gui.get_left_element(target, main_frame_name)
+        if frame then
+            Gui.destroy(frame)
+        end
     end)
 end
 
